d66d0a8b763fe8faa4a3bf85649c4edd35099912
Gustavo Sudre <gsudre@pobox.com>
2013-04-15 11:43:52 -0400

    Script to make running Freesurfer from DICOMS faster

---
+++ run_freesurfer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script that runs Freesurfer new subjects. Takes subject MEG code and MPRage folder as parameters
+#
+# Gustavo Sudre, 04/2013
+
+subj=$1
+mprageDir=$1
+AFNI_folder="/mnt/neuro/MEG_structural/afni"
+spacing=5
+model_script="$HOME/research_code/MEG_sensor2mneSourceSpace.sh"
+
+mkdir "$AFNI_folder"/"$subj"/
+cd $mprageDir
+to3d -prefix anat -session "$AFNI_folder"/"$subj"/*.dcm
+cd /tmp
+3dcopy "$AFNI_folder"/"$subj"/anat+orig "$subj"_anat.nii
+bash $model_script $subj $spacing "$subj"_anat.nii
+mv "$subj"_mne_model.log "$subj"_surface.log $SUBJECTS_DIR/$subj/
+mv "$subj"_anat.nii $AFNI_folder/$subj/
+
