763f39183f3466c782648fc31cf093c56cc8c740
Gustavo Sudre <gsudre@pobox.com>
2013-04-15 13:31:43 -0400

    Small bugs

--- run_freesurfer.sh
+++ run_freesurfer.sh
@@ -4,14 +4,14 @@
 # Gustavo Sudre, 04/2013
 
 subj=$1
-mprageDir=$1
+mprageDir=$2
 AFNI_folder="/mnt/neuro/MEG_structural/afni"
 spacing=5
 model_script="$HOME/research_code/MEG_sensor2mneSourceSpace.sh"
 
-mkdir "$AFNI_folder"/"$subj"/
+mkdir $AFNI_folder/$subj/
 cd $mprageDir
-to3d -prefix anat -session "$AFNI_folder"/"$subj"/*.dcm
+to3d -prefix anat -session $AFNI_folder/$subj/ *.dcm
 cd /tmp
 3dcopy "$AFNI_folder"/"$subj"/anat+orig "$subj"_anat.nii
 bash $model_script $subj $spacing "$subj"_anat.nii
