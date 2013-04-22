b514eeb03272033124dd75092f344c69ec905460
Gustavo Sudre <gsudre@pobox.com>
2013-04-22 11:10:25 -0400

    Updating variable names

--- combine_pli_perms.py
+++ combine_pli_perms.py
@@ -10,13 +10,13 @@ num_bands = 5
 num_labels = 68
 
 res = np.load(env.results + 'selected_voxels_chl.5_lp58_hp.5_th3500e15.npz')
-subj_voxels = res['subj_voxels'][()]
+subj_voxels = res['selected_voxels'][()]
 subjs = subj_voxels.keys()
 
 num_subj = len(subjs)
 rand_plis = np.zeros([num_perm, num_subj, num_bands, num_labels, num_labels])
 for s, subj in enumerate(subjs):
-    print('Subject {}/{}').format(s + 1, num_subjs)
+    print('Subject {}/{}').format(s + 1, num_subj)
     rand_files = glob.glob(env.results + 'rand_' + str(perm_blocks) + '_plis_' + subj + '_*.npz')
     cnt = 0
     for file in rand_files:
