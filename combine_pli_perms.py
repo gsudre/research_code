6d65c71fca87c616a1e1cd02fab7d46e1d003e59
Gustavo Sudre <gsudre@pobox.com>
2013-04-22 16:36:13 -0400

    Saving a dictionary

--- combine_pli_perms.py
+++ combine_pli_perms.py
@@ -14,15 +14,17 @@ subj_voxels = res['selected_voxels'][()]
 subjs = subj_voxels.keys()
 
 num_subj = len(subjs)
-rand_plis = np.empty([num_perm, num_subj, num_bands, num_labels, num_labels])
-rand_plis[:] = np.NaN
+rand_plis = []
 for s, subj in enumerate(subjs):
     print('Subject {}/{}').format(s + 1, num_subj)
+    subj_rand_pli = np.empty([num_perm, num_bands, num_labels, num_labels])
+    subj_rand_pli[:] = np.NaN
     rand_files = glob.glob(env.results + 'rand_' + str(perm_blocks) + '_plis_' + subj + '_*.npz')
     cnt = 0
     for file in rand_files:
         if cnt < num_perm:
             res = np.load(file)
             tmp = res['rand_plis'][()]
-            rand_plis[cnt:(cnt + perm_blocks), s, :, :, :] = tmp
+            subj_rand_pli[cnt:(cnt + perm_blocks), :, :, :] = tmp
             cnt += perm_blocks
+    rand_plis[subj] = subj_rand_pli
