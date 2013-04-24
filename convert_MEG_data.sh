d69a9600089b7b4ea40a6c4f042ece82242ff9cb
Gustavo Sudre <gsudre@pobox.com>
2013-04-24 12:45:36 -0400

    New script to convert DS to FIFF, individual subjects

---
+++ convert_MEG_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Script that converts .ds data to .fif for specific subjects. Takes in ds folder and destination folder
+#
+# Gustavo Sudre, 01/2013
+
+dsDir=$1
+outDir=$2
+
+splitUnderscore=(`echo $dsDir | tr '_' ' '`)
+# when we split with the underscore, the third to last item is the task, and the one before that, when we split on /, the last item is the subject name
+fileLength=${#splitUnderscore[@]}                                          
+last2P=$((fileLength - 3))
+task=${splitUnderscore[${last2P}]}
+
+restOfIt=$((fileLength - 4))
+splitDash=(`echo ${splitUnderscore[$restOfIt]} | tr '/' ' '`)
+fileLength=${#splitDash[@]}                                          
+lastP=$((fileLength - 1))
+subjectCode=${splitDash[${lastP}]}
+
+mne_ctf2fiff --ds $dsDir --fif tmp.fif
+mne_rename_channels --fif tmp.fif --alias ~/.mne/renameUPT001toSTI104.txt
+mne_process_raw --raw tmp.fif --projoff --lowpass 100 --decim 2 --grad 3 \
+				--save "$outDir"/"$subjectCode"_"$task"_LP100_CP3_DS300_raw.fif
+rm tmp.fif
