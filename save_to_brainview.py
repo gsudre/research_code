3791de5e8eb2710369c43d209a047da459820e40
Gustavo Sudre <gsudre@pobox.com>
2013-05-31 15:34:36 -0400

    Functions for brain 3D plotting are ready, with smoothing.

--- save_to_brainview.py
+++ save_to_brainview.py
@@ -14,18 +14,3 @@ def write_bv(X, filename):
 
     # write the matrix with the headr from above
     np.savetxt(filename, X, delimiter=" ", header=header.rstrip(), comments='', fmt='%.4f')
-
-
-def populate_matrix(X, num_vertex, idx, default=0):
-    ''' Returns a matrix of size num_vertex by X.shape[1], which is created using the elements from X that are placed in the positions in the new matrix gudied by the vector idx. default defines the starting value of the matrix. '''
-
-    if len(idx) != X.shape[0]:
-        print 'Error: there should be one index per row of X!'
-        return
-
-    X2 = np.zeros([num_vertex, X.shape[1]])
-    if default != 0:
-        X2[:] = default
-
-    X2[idx, :] = X
-    return X2
