410789a6a6a021a19bac20b4a46fb79e552c6db1
Gustavo Sudre <gsudre@pobox.com>
2013-07-05 18:35:05 -0400

    Almost done with script to check files in the server against table

--- lab_mgmt/check_MR_data_to_excel.py
+++ lab_mgmt/check_MR_data_to_excel.py
@@ -1,102 +1,124 @@
 ''' Puts a Y in every column in excel for which we have data in the server, and also creates the scan data sheet to be imported into labmatrix '''
 
-import openpyxl
 import glob
 from datetime import datetime
+import csv
 
 
-def check_for_data(dtype, date, folder):
-    mask_ids = glob.glob(folder + '/*')
-    for mask in mask_ids:
-        dirs = glob.glob(mask + '/*')
-        dir_num = [d for d, path in enumerate(dirs) if (path.find(date.strftime('%Y_%m_%d')) > 0) or (path.find(date.strftime('%Y%m%d')) > 0)]
-        if len(dir_num) > 0:
-            # we found a directory with the specific date inside the mask id
-            for d in dir_num:
-                # now, get the name of the text file
-                txtfile = glob.glob(dirs[d] + '/README*.txt')
-                fid = open(txtfile[0])
-                text = fid.read()
-                pos = text.lower().find(dtype)
-                if pos > 0:
-                    return 1
-
-    # if we get to here, we didn't find the type fo data in any of the matching folders
-    print('Data {} not found on {} in {}').format(dtype, date.strftime('%Y_%m_%d'), folder)
-    return -1
-
-
-def check_date(date, folder):
-    mask_ids = glob.glob(folder + '/*')
-    for mask in mask_ids:
-        dirs = glob.glob(mask + '/*')
-        dir_num = [d for d, path in enumerate(dirs) if (path.find(scan_date.strftime('%Y_%m_%d')) > 0) or (path.find(scan_date.strftime('%Y%m%d')) > 0)]
-        if len(dir_num) > 0:
-            # we found a directory with the specific date inside the mask id
-            return True
-    return False
+def check_for_data(dtype, folder):
+    # now, get the name of the text file
+    txtfile = glob.glob(folder + '/README*.txt')
+    fid = open(txtfile[0])
+    text = fid.read()
+    pos = text.lower().find(dtype)
+    return (pos > 0)
 
 
 path = '/Volumes/neuro/MR_data/'
-
-has = {}
+max_date = datetime.strptime('20130614', '%Y%m%d')  # last date we entered stuff in the spreadsheet. After that it went in Labmatrix
 
 # open spreadsheet
 fname = r'/Users/sudregp/tmp/visit_records.xlsx'
+csv_table_out = '/Users/sudregp/tmp/scan_table.csv'
 
 list_mod = ['rage', 'fmri', 'rest', 'edti', 'clinical', 'fat_sat']  # note that these names need to be found in the README file!
-mod_in_excel = ['W', 'X', 'Z', 'AA', 'V', 'AC']
 scan_headers = ['MRN', 'Type', 'Scanner', 'Mask ID', 'Task / MEG ID', 'Notes', 'MPRAGE Quality']
-# rest,stop task,MPRAGE,clinical,T2,eDTI
-# 3TA,3TB,1.5,MEG 
+scan_table = []
+scan_table.append(scan_headers)
 
-from openpyxl.reader.excel import load_workbook
 wb = load_workbook(filename=fname)
 ws = wb.worksheets[0]
 
 # get a list of the subjects in the server
 subjs = glob.glob(path + '/*')
 
+errors = []
+print "Add these rows to the Excel spreadsheet:"
+
 # for each subject in the server
-for row_idx in range(2, ws.get_highest_row()):
-    mrn = str(ws.cell('A' + str(row_idx)).value)
-    last_name = ws.cell('E' + str(row_idx)).value
-    first_name = ws.cell('F' + str(row_idx)).value
-    scan_date = ws.cell('I' + str(row_idx)).value
+for subj_dir in subjs:
+    mrn = int(subj_dir.split('/')[-1].split('-')[-1])
+    # look for all mask ids this subject has
+    maskids = glob.glob(subj_dir + '/*')
+    # for each mask id, figure out the date of scan so we can check the spreadsheet and create a new scan record
+    for maskid_dir in maskids:
+        mask_id = int(maskid_dir.split('/')[-1])
+        date_dir = glob.glob(maskid_dir + '/2*')[0]
+        date = datetime.strptime(date_dir.split('/')[-1].split('-')[0].replace('_', ''), '%Y%m%d')
 
-    # if names have a - or space, replace by _
-    last_name = last_name.replace('-', '_')
-    first_name = first_name.replace('-', '_')
-    last_name = last_name.replace(' ', '_')
-    first_name = first_name.replace(' ', '_')
-    last_name = last_name.replace('\'', '_')
+        if date < max_date:
+            # looking for this particular entry in the spreadsheet
+            row_idx = [i for i in range(1, ws.get_highest_row()) if (mrn == ws.cell('A' + str(i)).value) and (date == ws.cell('B' + str(i)).value)]
 
-    for m, mod in enumerate(list_mod):
-        has[mod] = (ws.cell(mod_in_excel[m] + str(row_idx)).value == 'Y')
+            # we'll need to output to the screen the new subjects to go into the visit record, so that we can merge it with the visit record later. Saving the excel file screws up the dates.
+            new_visit = []
+            if len(row_idx) == 0:
+                add_new = True
+                new_visit.append(str(mrn))
+                new_visit.append(str(date.strftime('%m/%d/%y')))
+                for i in range(4):
+                    new_visit.append('')
+            else:
+                add_new = False
 
-    # check whether we have a folder for the subject
-    dir_num = [l for l, folder in enumerate(subjs)
-               if (folder.find(mrn) > 0 and
-                   folder.find(last_name.upper()) > 0 and
-                   folder.find(first_name.upper()) > 0)]
+            # check what kinds of data this subject has in this mask id. Create a new Scan table entry for each data type found
+            if check_for_data('clinical', date_dir):
+                scan_table.append([mrn, 'clinical', '3TA', mask_id, '', '', ''])
+                new_visit.append('Y')
+            else:
+                new_visit.append('N')
+            # if not add_new and ws.cell('G' + str(row_idx[0])).value != new_visit[-1]:
+            #     errors.append('%d on %s should be %c' % (mrn, str(date.strftime('%m/%d/%y')), new_visit[-1]))
 
-    # check whether the subject scanned
-    scanned = ws.cell('L' + str(row_idx)).value != -1
+            if check_for_data('rage', date_dir):
+                scan_table.append([mrn, 'MPRAGE', '3TA', mask_id, '', '', ''])
+                new_visit.append('Y')
+            else:
+                new_visit.append('N')
+            if not add_new and ws.cell('H' + str(row_idx[0])).value != new_visit[-1]:
+                errors.append('%d on %s should be %c' % (mrn, str(date.strftime('%m/%d/%y')), new_visit[-1]))    
 
-    if len(dir_num) == 0 and scanned:
-        # subject not found in the server. Check if we expected fMRI data
-        missing = [mode for mode, val in has.iteritems() if val]
-        if len(missing) > 0:
-            print('Subject {}, {} ({}) not in the server. On {}, expected: {}').format(last_name, first_name, mrn, scan_date.strftime('%Y_%m_%d'), missing)
-    elif scanned:
-        # subject is in the server. First, check whether we have that particular scan for the subject
+            if check_for_data('fmri', date_dir):
+                scan_table.append([mrn, 'stop task', '3TA', mask_id, '', '', ''])
+                new_visit.append('Y')
+            else:
+                new_visit.append('N')
+            if not add_new and ws.cell('I' + str(row_idx[0])).value != new_visit[-1]:
+                errors.append('%d on %s should be %c' % (mrn, str(date.strftime('%m/%d/%y')), new_visit[-1]))
 
-        if check_date(scan_date, subjs[dir_num[0]]):
-            # if we do, then look for the types of data listed in the spreadsheet
-            for mod in list_mod:
-                if has[mod]:
-                    check_for_data(mod, scan_date, subjs[dir_num[0]])
-        else:
-            missing = [mode for mode, val in has.iteritems() if val]
-            if len(missing) > 0:
-                print('No scans on {} for {}, {} ({}). Expected: {}').format(scan_date.strftime('%Y_%m_%d'), last_name, first_name, mrn, missing)
+            if check_for_data('rest', date_dir):
+                scan_table.append([mrn, 'rest', '3TA', mask_id, '', '', ''])
+                new_visit.append('Y')
+            else:
+                new_visit.append('N')
+            if not add_new and ws.cell('J' + str(row_idx[0])).value != new_visit[-1]:
+                errors.append('%d on %s should be %c' % (mrn, str(date.strftime('%m/%d/%y')), new_visit[-1]))
+
+            if check_for_data('edti', date_dir):
+                scan_table.append([mrn, 'eDTI', '3TA', mask_id, '', '', ''])
+                new_visit.append('Y')
+            else:
+                new_visit.append('N')
+            if not add_new and ws.cell('K' + str(row_idx[0])).value != new_visit[-1]:
+                errors.append('%d on %s should be %c' % (mrn, str(date.strftime('%m/%d/%y')), new_visit[-1]))
+
+            if check_for_data('fat_sat', date_dir):
+                scan_table.append([mrn, 'T2', '3TA', mask_id, '', '', ''])
+                new_visit.append('Y')
+            else:
+                new_visit.append('N')
+            # if not add_new and ws.cell('L' + str(row_idx[0])).value != new_visit[-1]:
+            #     errors.append('%d on %s should be %c' % (mrn, str(date.strftime('%m/%d/%y')), new_visit[-1]))
+
+            if add_new:
+                print "\t".join(new_visit)
+
+fid = open(csv_table_out, 'w')
+wr = csv.writer(fid)
+wr.writerows(scan_table)
+fid.close()
+
+print '\nThese assignments are incorrect in the Excel matrix:'
+print errors
+
+# NEED TO WRITE THE CORRECTED STUFF TO A FILE INSTEAD OF TO SCREEN TO BE LATER MERGED TO EXCEL. OR JUST RE-WRITE A NEW TAB FILE. JUST DON'T REWRITE THE EXCEL BECAUSE IT SCREWS UP THE DATES
