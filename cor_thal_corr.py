8c05a59a4d1f9c0925108b74b0639073ca5de7ea
Gustavo Sudre <gsudre@pobox.com>
2013-07-11 11:40:23 -0400

    Doing more regular correlation analysis

--- cor_thal_corr.py
+++ cor_thal_corr.py
@@ -3,14 +3,14 @@ import env
 import scipy
 from scipy import stats
 
-cortex = np.genfromtxt(env.data + '/structural/cortex_SA_NV_10to21.csv', delimiter=',')
+cortex = np.genfromtxt(env.data + '/structural/cortexR_SA_NV_10to21_2closestTo16.csv', delimiter=',')
 # removing first column and first row, because they're headers
 cortex = scipy.delete(cortex, 0, 1)
 cortex = scipy.delete(cortex, 0, 0)
 # format it to be subjects x variables
 cortex = cortex.T
 
-subcortex = np.genfromtxt(env.data + '/structural/thalamus_SA_NV_10to21.csv', delimiter=',')
+subcortex = np.genfromtxt(env.data + '/structural/thalamusR_SA_NV_10to21_2closestTo16.csv', delimiter=',')
 # removing first column and first row, because they're headers
 subcortex = scipy.delete(subcortex, 0, 1)
 subcortex = scipy.delete(subcortex, 0, 0)
@@ -18,8 +18,8 @@ subcortex = scipy.delete(subcortex, 0, 0)
 subcortex = subcortex.T
 
 # selecting only a few vertices in the thalamus
-my_sub_vertices = [2310, 1574, 1692, 1262, 1350]
-my_sub_vertices = [1533, 1106, 225, 163, 2420, 2966, 1393, 1666, 1681, 1834, 2067]  # GS made it up by looking at anamoty, refer to Evernote for details
+# my_sub_vertices = [2310, 1574, 1692, 1262, 1350]
+# my_sub_vertices = [1533, 1106, 225, 163, 2420, 2966, 1393, 1666, 1681, 1834, 2067]  # GS made it up by looking at anamoty, refer to Evernote for details
 my_sub_vertices = range(subcortex.shape[1])
 
 num_subjects = cortex.shape[0]
@@ -34,4 +34,4 @@ for x in range(X.shape[1]):
         corr[x, y], pvals[x, y] = stats.pearsonr(X[:, x], Y[:, y])
 
 
-np.savez(env.results + 'structurals_pearson_all_thalamus_all_cortex', corr=corr, pvals=pvals, my_sub_vertices=my_sub_vertices)
+np.savez(env.results + 'structurals_pearson_all_thalamus_all_cortex_2closestTo16', corr=corr, pvals=pvals, my_sub_vertices=my_sub_vertices)
