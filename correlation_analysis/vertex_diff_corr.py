93f9a6b04ab73c2f1b5bde65169c09b1726f1fad
Gustavo Sudre <gsudre@pobox.com>
2013-10-03 11:59:22 -0400

    Changed to lists to use less memory

--- correlation_analysis/vertex_diff_corr.py
+++ correlation_analysis/vertex_diff_corr.py
@@ -4,6 +4,7 @@ import scipy
 import matplotlib.mlab as mlab
 import pdb
 from scipy import stats
+import pdb as pb
 
 
 def load_structural(fname):
@@ -41,18 +42,20 @@ def load_rois(fname, brain):
     return roi_labels, roi_vertices
 
 
-def construct_matrix(data, rois, roi_labels, hemi, idx):
+def construct_matrix(data, rois, roi_labels, hemi):
 # Reorganizes the vertices by ROI
-    Y = np.empty([np.sum(idx), data.shape[1]])
+    Y = np.empty([data.shape[0], init_verts],dtype=np.float16)
+    Y[:] = np.nan
     verts = []
     cnt = 0
     for r, roi in enumerate(rois):
         verts_in_roi = len(roi)
         if verts_in_roi > 0:
-            Y[:,cnt:(cnt+verts_in_roi)] = data[idx,roi]
+            Y[:,cnt:(cnt+verts_in_roi)] = data[:,roi]
             cnt += verts_in_roi
-            tmp_verts = [roi_labels[r] for i in range(verts_in_roi)]
+            tmp_verts = [hemi + '_' + roi_labels[r] for i in range(verts_in_roi)]
             verts += tmp_verts
+    Y = Y[:Y.shape[0], :len(verts)]
     return Y, verts
 
 
@@ -73,69 +76,48 @@ cortex_labels = [['Occipital', [132, 38, 63, 97, 175, 112, 251, 98, 154, 37, 54,
                 ['Frontal', [10, 2, 75, 5, 6, 1, 7, 70, 50, 15, 80, 90, 85, 27]],
                 ['Cingulate', [7, 27]]]
 
-group = ['NV', 'persistent', 'remission']
+group = 'NV' #, 'persistent', 'remission']
 brain = ['striatum', 'gp', 'cortex', 'thalamus']
 hemi = ['L', 'R']
-big = 10^5
+time = ['baseline', 'last']
+init_verts = 10e5
+init_subjs = 100
 
-base_corrs = []
-last_corrs = []
-delta_corrs = []
+all_corrs = []
 matched_gf = np.recfromcsv('/Users/sudregp/data/structural/gf_1473_matched_on18.csv')
-for g in group:
-    idx = np.logical_and(matched_gf['group3']==('"""%s"""'%g),matched_gf['match_outcome']==1)
-    idx_base = np.zeros((len(idx)), dtype=bool)
-    idx_last = np.zeros((len(idx)), dtype=bool)
-    # find out all scans for each unique subject
-    subjects = np.unique(matched_gf[idx]['personx'])  # only look at subjects that obeyed previous criteria
-    cnt=0
-    for subj in subjects:
-        good_subj_scans = np.nonzero(np.logical_and(matched_gf['personx']==subj,idx))[0]
-        # only use subjects with one scan < 18 and another after 18
-        ages = matched_gf[good_subj_scans]['agescan']
-        if len(ages) > 1 and np.min(ages)<18 and np.max(ages) > 18:
-            print ages
-            ages = np.argsort(ages)
-            # makes the first scan true
-            idx_base[good_subj_scans[ages[0]]] = True
-            # makes the last scan true
-            idx_last[good_subj_scans[ages[-1]]] = True
-            cnt+=1
-
-    num_subjects = np.sum(np.logical_and(idx,idx_base))
+for t in time:
     # create huge array so we can add all the data and thenresize it appropriately
-    raw_base = np.empty([num_subjects, big])
-    raw_last = np.empty([num_subjects, big])
+    raw = np.empty([init_subjs, init_verts],dtype=np.float16)
+    raw[:] = np.nan
     # verts will be a list of strings, with the name of the ROI the vertex belongs to
     verts = []
+    cnt = 0
     for b in brain:
         print 'Working on ' + b
         for h in hemi:
-
             data_roi_labels, data_roi_verts = load_rois('%s/data/structural/labels/%s_%s_labels.txt'%(os.path.expanduser('~'), b, h), b)
-            data = load_structural('%s/data/structural/baseline_%s%s_SA_%s_QCCIVETlt35_QCSUBePASS_MATCHSCRIPT_lt18.csv' % (os.path.expanduser('~'), b, h, g))
-            X, vert_labels = construct_matrix(data, data_roi_verts, data_roi_labels, h, np.logical_and(idx,idx_base))
-            raw_base = np.hstack((raw_base, X))
+            if t == 'baseline':
+                data = load_structural('%s/data/structural/%s_%s%s_SA_%s_QCCIVETlt35_QCSUBePASS_MATCHSCRIPT_lt18.csv' % (os.path.expanduser('~'), t, b, h, group))
+            else:
+                data = load_structural('%s/data/structural/%s_%s%s_SA_%s_QCCIVETlt35_QCSUBePASS_MATCHSCRIPT_mt18.csv' % (os.path.expanduser('~'), t, b, h, group))
+            num_subjects = data.shape[0]
+            X, vert_labels = construct_matrix(data, data_roi_verts, data_roi_labels, h)
+            raw[0:num_subjects,cnt:(cnt+len(vert_labels))] = X
+            cnt += len(vert_labels)
             # verts are the same for both baseline and last scan
-            verts = np.hstack((verts, vert_labels))
-
-            ## now we add in the same subjects' last scan
-            #data = load_structural('%s/data/structural/last_%s%s_SA_%s_QCCIVETlt35_QCSUBePASS_MATCHSCRIPT_mt18.csv' % (os.path.expanduser('~'), b, h, g))
-            #X, vert_labels = construct_matrix(data, data_roi_verts, idx)
-            #raw_last = np.hstack((raw_last, X))
+            verts = verts + vert_labels
+    # trimming the matric to the correct number of vertices and subjects
+    print 'Resizing matrices'
+    num_vertices = len(verts)
+    raw = raw[:num_subjects, :num_vertices]
 
     # when we're done gathering all the data, compute the correlations
     print 'Computing correlations\n'
-    num_vertices = raw_base.shape[1]
-    corr_base = np.empty((num_vertices,num_vertices))
-    corr_last = np.empty((num_vertices,num_vertices))
-    corr_base[:] = np.NAN
-    corr_last[:] = np.NAN
+    num_vertices = raw.shape[1]
+    corrs = []
     for x in range(num_vertices):
-        print 'vertex', x+1, '/', num_vertices
-        for y in range(x+1,num_vertices):
-            corr_base[x, y] = scipy.stats.pearsonr(raw_base[:, x], raw_base[:, y])
-            corr_last[x, y] = scipy.stats.pearsonr(raw_last[:, x], raw_last[:, y])
-    base_corrs.append(corr_base)
-    last_corrs.append(corr_last)
-    delta_corrs.append(corr_last-corr_base)
+        print 'vertex', x, '/', num_vertices
+        for y in range(x, num_vertices):
+            corrs.append(np.float16(scipy.stats.pearsonr(raw[:, x], raw[:, y])[0]))
+    all_corrs.append(corrs)
+np.save('%s/data/results/structural/verts_corr_%s'%(os.path.expanduser('~'), group), all_corrs)
