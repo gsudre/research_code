ca6438ff9e123637d3f0aa5871ba058286331afb
Gustavo Sudre <gsudre@pobox.com>
2013-11-12 14:10:30 -0500

    Not reporting duplicate MRNs anymore

--- lab_mgmt/mrn_crawler.py
+++ lab_mgmt/mrn_crawler.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 from xlrd import open_workbook
 
 mrnListFile = '/Users/sudregp/tmp/mrnList.txt'
-dataDir = '/Users/sudregp/tmp/'
+dataDir = '/Volumes/Labs/Shaw/'
 
 # get the list of MRNs
 mrns = [] 
@@ -20,6 +20,7 @@ for dirName, subdirList, fileList in os.walk(dataDir):
         # For CSV and TXT files, just search each line for an occurrence of each of the MRNs.
         if fname.lower().find('.csv') > 0 or fname.lower().find('.txt') > 0:
             print 'Crawling', fullName
+            foundMRNs = []
             # let's ignore files that are too big, because they probably contain data, not metadata
             if os.path.getsize(fullName) > 100000000:
                 print '\tToo big'
@@ -27,14 +28,16 @@ for dirName, subdirList, fileList in os.walk(dataDir):
                 fid = open(fullName, 'r')
                 for line in fid:
                     for mrn in mrns:
-                        if line.find(str(mrn)) >= 0:
+                        if line.find(str(mrn)) >= 0 and mrn not in foundMRNs:
                             print '\tFound', mrn
+                            foundMRNs.append(mrn)
                 fid.close()
         # The idea for XLS and XLSX is the same, but we need to use different libraries. We just iterate over all sheets, and all cells in the sheet. If we can convert it to an integer, then we check if it's one of our MRNs
         elif fname.lower().find('.xlsx') > 0:
             try:
                 wb = load_workbook(filename = fullName, use_iterators = True)
                 print 'Crawling', fullName
+                foundMRNs = []
                 sheets = wb.get_sheet_names()
                 for sheet in sheets:
                     ws = wb.get_sheet_by_name(sheet)
@@ -45,14 +48,16 @@ for dirName, subdirList, fileList in os.walk(dataDir):
                             except:
                                 thisMRN = None
                             for mrn in mrns:
-                                if mrn == thisMRN:
+                                if mrn == thisMRN and mrn not in foundMRNs:
                                     print '\tFound', mrn
+                                    foundMRNs.append(mrn)
             except:
                 print 'ERROR: Could not open', fullName, '(password protected?)'
         elif fname.lower().find('.xls') > 0:
             try: 
                 book = open_workbook(fullName)
                 print 'Crawling', fullName
+                foundMRNs = []
                 for sheet in book.sheets():
                     for row_index in range(sheet.nrows):
                         for cell in sheet.row(row_index):
@@ -61,7 +66,8 @@ for dirName, subdirList, fileList in os.walk(dataDir):
                             except:
                                 thisMRN = None
                             for mrn in mrns:
-                                if mrn == thisMRN:
+                                if mrn == thisMRN and mrn not in foundMRNs:
                                     print '\tFound', mrn
+                                    foundMRNs.append(mrn)
             except:
                 print 'ERROR: Could not open', fullName
