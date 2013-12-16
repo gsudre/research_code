539c83fbf75f77b7352cecad01563fb850717369
Gustavo Sudre <gsudre@pobox.com>
2013-12-16 13:23:12 -0500

    Added FU data to NVonly

--- correlation_analysis/simple_method/create_matrices_NVonly.R
+++ correlation_analysis/simple_method/create_matrices_NVonly.R
@@ -3,7 +3,6 @@
 # ESthresh by nperms
 
 source('~/research_code/correlation_analysis/macacc_massage_data_matched_diff.R')
-source('~/research_code/correlation_analysis/compile_baseline.R')
 
 getESfromR <- function(m1, m2) {
     a = cbind(m1, m2)
@@ -41,16 +40,43 @@ thresh = seq(.2,1,.1)
 nverts = dim(thalamusR)[2]
 perm_dists = vector(mode='numeric',length=length(thresh))
 
+# source('~/research_code/correlation_analysis/compile_baseline.R')
+# for (p in 1:nperms) {
+#     cat(p,'\n')
+#     idx = which(gfBase$DX=='NV')
+#     perm_labels <- sample.int(length(idx), replace = FALSE)
+#     gidx1 = idx[perm_labels[1:64]]
+#     es1 = getESfromR(thalamusRBase[gidx1,], striatumRBase[gidx1,])
+#     gidx2 = idx[perm_labels[65:96]]
+#     es2 = getESfromR(thalamusRBase[gidx2,], striatumRBase[gidx2,])
+#     gidx3 = idx[perm_labels[97:128]]
+#     es3 = getESfromR(thalamusRBase[gidx3,], striatumRBase[gidx3,])
+#     for (i in 1:length(thresh)) {
+#         bes1 = binarize(abs(es1),thresh[i])
+#         bes2 = binarize(abs(es2),thresh[i])
+#         bes3 = binarize(abs(es3),thresh[i])
+#         d1 = length(setdiff(which(bes1),union(which(bes2),which(bes3))))/sum(bes1)
+#         d2 = length(setdiff(which(bes2),union(which(bes1),which(bes3))))/sum(bes2)
+#         d3 = length(setdiff(which(bes3),union(which(bes2),which(bes1))))/sum(bes3)
+#         perm_dists[i] = max(d1,d2,d3)
+#     }
+#     write(perm_dists,
+#           file='~/data/results/structural_v2/perm_dists_NVAllBaseOnly_thalamusRstriatumR.txt',
+#           ncolumns=length(perm_dists),
+#           append=T)
+# }
+
+source('~/research_code/correlation_analysis/compile_last.R')
 for (p in 1:nperms) {
     cat(p,'\n')
     idx = which(gfBase$DX=='NV')
     perm_labels <- sample.int(length(idx), replace = FALSE)
     gidx1 = idx[perm_labels[1:64]]
-    es1 = getESfromR(thalamusRBase[gidx1,], striatumRBase[gidx1,])
+    es1 = getESfromR(thalamusRLast[gidx1,], gpRLast[gidx1,])
     gidx2 = idx[perm_labels[65:96]]
-    es2 = getESfromR(thalamusRBase[gidx2,], striatumRBase[gidx2,])
+    es2 = getESfromR(thalamusRLast[gidx2,], gpRLast[gidx2,])
     gidx3 = idx[perm_labels[97:128]]
-    es3 = getESfromR(thalamusRBase[gidx3,], striatumRBase[gidx3,])
+    es3 = getESfromR(thalamusRLast[gidx3,], gpRLast[gidx3,])
     for (i in 1:length(thresh)) {
         bes1 = binarize(abs(es1),thresh[i])
         bes2 = binarize(abs(es2),thresh[i])
@@ -61,7 +87,7 @@ for (p in 1:nperms) {
         perm_dists[i] = max(d1,d2,d3)
     }
     write(perm_dists,
-          file='~/data/results/structural_v2/perm_dists_NVAllBaseOnly_thalamusRstriatumR.txt',
+          file='~/data/results/structural_v2/perm_dists_NVAllLastOnly_thalamusRgpR.txt',
           ncolumns=length(perm_dists),
           append=T)
 }
