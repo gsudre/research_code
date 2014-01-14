914f33ff12eb4eaa43943b318ad6b38f89d70833
Gustavo Sudre <gsudre@pobox.com>
2014-01-14 17:31:35 -0500

    Added contrasts

--- fmri/run_afni_proc.sh
+++ fmri/run_afni_proc.sh
@@ -3,7 +3,7 @@ echo "Enter the subject mask ID (4 digits), followed by [ENTER]:"
 read subj
 
 subj_dir=/mnt/neuro/data_by_maskID/$subj/afni
-stim_dir=/mnt/neuro/MR_behavioral/stim_files_analysis1
+stim_dir=/mnt/neuro/MR_behavioral/stop_task_analysis/stim_files_analysis1
 
 afni_proc.py -dsets ${subj_dir}/stop_run1+orig.BRIK           \
     ${subj_dir}/stop_run2+orig.BRIK                   \
@@ -23,9 +23,10 @@ afni_proc.py -dsets ${subj_dir}/stop_run1+orig.BRIK           \
     -align_opts_aea -cost lpc+zz -AddEdge                   \
     -regress_basis 'BLOCK(1,1)'                     \
     -regress_censor_motion 1                   \
+    -regress_censor_outliers 0.1                             \
     -regress_est_blur_epits                         \
     -regress_est_blur_errts                         \
-    -regress_no_fitts                           \
+    -regress_compute_fitts                           \
     -regress_stim_times                         \
         $stim_dir/${subj}_STG-correct.txt       \
         $stim_dir/${subj}_STG-incorrect.txt       \
@@ -36,14 +37,19 @@ afni_proc.py -dsets ${subj_dir}/stop_run1+orig.BRIK           \
                 STI-correct              \
                 STI-incorrect             \
     -regress_opts_3dD       -bout                       \
-                -tout                       \
-                -local_times                    \
-                -GOFORIT 99                                     \
-                -allzero_OK                                     \
-                -jobs 3                                         \
-                -noFDR                                          \
+        -tout                       \
+        -local_times                    \
+        -GOFORIT 99                                     \
+        -allzero_OK                                     \
+        -jobs 3                                         \
+        -noFDR                                          \
+        -gltsym 'SYM: STI-correct -STI-incorrect' -glt_label 1   \
+                STI-correct_VS_STI-incorrect                     \
+        -gltsym 'SYM: STG-correct -STI-correct' -glt_label 2     \
+                STG-correct_VS_STI-correct                       \
+        -gltsym 'SYM: STG-correct -STI-incorrect' -glt_label 3   \
+                STG-correct_VS_STI-incorrect                     \
     -regress_opts_reml  -GOFORIT 99                 \
     -regress_reml_exec   
 
 tcsh -xef $subj_dir/stop.proc.$subj | tee $subj_dir/output.stop.proc.$subj
-# -tcat_remove_first_trs 4                       \
