8ee9ec7b7e508e89b820ca72cc42e690b32d8df1
Gustavo Sudre <gsudre@pobox.com>
2014-03-07 18:31:14 -0500

    Fixing a few minor bugs

--- convert_all_MEG_data.sh
+++ convert_all_MEG_data.sh
@@ -2,9 +2,11 @@
 # Script that converts .ds data to .fif
 #
 # Gustavo Sudre, 01/2013
+# Updated in 03/2014 to add log file
 
 dataDir='/mnt/neuro/MEG_data/raw/'
 outDir='/mnt/neuro/MEG_data/fifs/'
+log_file='/Users/sudregp/fifs.log'
 task='rest'
 
 dates=`ls $dataDir`
@@ -18,11 +20,20 @@ do
 	do
 		splitTask=(`echo $f | tr '_' ' '`)
 		splitRaw=(`echo $f | tr '-' ' '`)
-		if [ ${splitTask[1]} == $task ] && [ ${splitRaw[0]} == $f ]; then
+		fname="$splitTask"_"$task"_LP100_CP3_DS300_raw.fif
+		echo Working on ${fname}
+		if [ -e ${outDir}/${fname} ]; then
+			echo "$splitTask",exists >> ${log_file}
+		elif [ ${splitTask[1]} == $task ] && [ ${splitRaw[0]} == $f ]; then
+			# note that we always convert the first version of the file. If we want the second one, we need to run the commands manually
 			mne_ctf2fiff --ds $dataDir/$d/$f/ --fif tmp.fif
 			mne_rename_channels --fif tmp.fif --alias ~/.mne/renameUPT001toSTI104.txt
-			mne_process_raw --raw tmp.fif --projoff --highpass 0.5 --lowpass 58 --decim 2 --grad 3 \
-				--save "$outDir"/"$splitTask"_"$task"_LP58_HP.5_CP3_DS300_raw.fif
+			mne_process_raw --raw tmp.fif --projoff --lowpass 100 --decim 2 --grad 3 --save ${outDir}/${fname}
+			if [ -e ${outDir}/${fname} ]; then
+				echo "$splitTask",success >> ${log_file}
+			else
+				echo "$splitTask",error >> ${log_file}
+			fi
 		fi
 	done
 done
