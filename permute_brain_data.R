03157b3ec751c09b93914f121c71b703cdc2f589
Gustavo Sudre <gsudre@pobox.com>
2014-03-25 14:47:36 -0400

    Now running without NVs

--- permute_brain_data.R
+++ permute_brain_data.R
@@ -1,8 +1,22 @@
 out_mask='~/data/tmp/perms/perm'
 gf = read.csv('~/tmp/FINAL_TSA_allClean149.csv')
-nperms = 3
-tmp = gf$SX_inattb
-brain_data = read.table('~/data/dti/all_FA_skeleton.txt')
+nperms = 20
+brain_data = read.table('~/data/dti/perVSrem_FA_skeleton.txt')
+data = gf$SX_inattb
+nsubj = dim(gf)[1]
+tmp = vector()
+for (i in 1:nsubj) {
+    if (gf[i,]$DX_GROUP=='persistent') {
+        tmp = c(tmp, data[i])
+    }
+}
+for (i in 1:nsubj) {
+    if (gf[i,]$DX_GROUP=='remitted') {
+        tmp = c(tmp, data[i])
+    }
+}
+# tmp = gf$SX_inattb
+
 ###
 set.seed( as.integer((as.double(Sys.time())*1000+Sys.getpid()) %% 2^31) )
 out = brain_data[,1:4]
@@ -19,6 +33,6 @@ for (p in 1:nperms) {
         stats[v] = summary(fit)$coefficients[2,3]
     }
     out[,4] = stats
-    fname = sprintf('%s_%s-%05f.RData', out_mask, Sys.info()["nodename"], runif(1, 1, 99999))
+    fname = sprintf('%s_%s-%05f.txt', out_mask, Sys.info()["nodename"], runif(1, 1, 99999))
     write.table(out, file=fname, col.names=F, row.names=F)
 }
