0f203af582a4e7e35177401151cd3d8fa4e42df5
Gustavo Sudre <gsudre@pobox.com>
2014-03-25 18:49:41 -0400

    Converting to TFCE inside the script

--- permute_brain_data.R
+++ permute_brain_data.R
@@ -1,4 +1,6 @@
-out_mask='~/data/tmp/perms/perm'
+out_mask='~/data/tmp/perms2/perm'
+data_dir = '~/data/dti/'
+nii_template = 'persistentVSremitted_FA_skeletonised.nii.gz'
 gf = read.csv('~/tmp/FINAL_TSA_allClean149.csv')
 nperms = 20
 brain_data = read.table('~/data/dti/perVSrem_FA_skeleton.txt')
@@ -27,12 +29,18 @@ for (p in 1:nperms) {
     print(sprintf('perm %d',p))
     perm_labels <- sample.int(length(tmp), replace = FALSE)
     randtmp = tmp[perm_labels]
-    for (v in 1:dim(data)[1]) {
+    for (v in 1:nvoxels) {
         fit = lm(data[v,]~randtmp)
         # CHECK THIS FOR EVERY MODEL!
         stats[v] = summary(fit)$coefficients[2,3]
     }
     out[,4] = stats
-    fname = sprintf('%s_%s-%05f.txt', out_mask, Sys.info()["nodename"], runif(1, 1, 99999))
-    write.table(out, file=fname, col.names=F, row.names=F)
+    fname = sprintf('%s_%s-%05f', out_mask, Sys.info()["nodename"], runif(1, 1, 99999))
+    write.table(out, file=sprintf('%s.txt', fname), col.names=F, row.names=F)
+    system(sprintf('cat %s.txt | 3dUndump -master %s/%s -datum float -prefix %s.nii.gz -overwrite -',
+                   fname, data_dir, nii_template, fname), wait=T)
+    system(sprintf('fslmaths %s.nii.gz -tfce 2 1 26 %s_tfce.nii.gz', fname, fname), wait=T)
+    system(sprintf('3dmaskdump -o %s_tfce.txt -overwrite -mask %s/mean_FA_skeleton_mask.nii.gz %s_tfce.nii.gz',
+                   fname, data_dir, fname), wait=T)
+    system(sprintf('rm %s.nii.gz %s_tfce.nii.gz', fname, fname), wait=T)
 }
