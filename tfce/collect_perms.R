720718565afac953024b40287c66a8b446aeedc1
Gustavo Sudre <gsudre@pobox.com>
2014-03-28 08:51:08 -0400

    separating some functions

---
+++ tfce/collect_perms.R
@@ -0,0 +1,38 @@
+mode = 'vox' # 'vox' or 'tfce'
+iter = 3
+res_name = 'pairwise'
+path = sprintf('~/data/results/tfce/perms%d/', iter)
+if (mode=='tfce') {
+    fname = sprintf('~/data/results/tfce/%s_tfce.txt', res_name)
+    pattern = 'tfce.txt$' # ends in tfce
+} else {
+    fname = sprintf('~/data/results/tfce/%s.txt', res_name)
+    pattern = '[0-9].txt$' # ends in a number
+}
+files = list.files(path, pattern=pattern)
+nperms = 10#length(files)
+nvoxels = 80689
+null_dist_corr = vector(length=nperms) 
+ps = vector(length=nperms, mode='integer')
+results = read.table(fname)
+out = results[,1:4]
+results = as.matrix(results[,4:dim(results)[2]])
+for (f in 1:nperms) {
+    cat(f, '/', nperms, '\n')
+    data = read.table(sprintf("%s/%s", path, files[f]))
+    data = as.matrix(data[,4:dim(data)[2]])
+    null_dist_corr[f] = max(data)
+    idx = which(data > results) 
+    ps[idx] = ps[idx] + 1
+}
+ps = ps/nperms
+out[,4] = 1-ps
+fname = sprintf('~/data/results/tfce/%s_%s_p.txt', res_name, mode)
+write.table(out, file=fname, col.names=F, row.names=F)
+corrps = ps  # just initializing it
+for (v in 1:nvoxels) {
+    corrps[v] = sum(null_dist_corr > results[v])/nperms
+}
+out[,4] = 1-corrps
+fname = sprintf('~/data/results/tfce/%s_%s_corrp.txt', res_name, mode)
+write.table(out, file=fname, col.names=F, row.names=F)
--- tfce/compute_stats.R
+++ tfce/compute_stats.R
@@ -1,23 +1,54 @@
 out_dir='~/data/results/tfce/'
 data_dir = '~/data/dti/'
-nii_template = 'all_FA_skeletonised.nii.gz'
+# nii_template = 'all_FA_skeletonised.nii.gz'
+# nii_template = 'persistentVSremitted_FA_skeletonised.nii.gz'
+nii_template = 'nvVSpersistent_FA_skeletonised.nii.gz'
 gf = read.csv('~/Documents/philip/dti/FINAL_TSA_allClean149.csv')
-tmp = gf$SX_inattb
-brain_data = read.table('~/data/dti/all_FA_skeleton.txt')
+
+# brain_data = read.table('~/data/dti/all_FA_skeleton.txt')
+# brain_data = read.table('~/data/dti/persistentVSremitted_FA_skeletonised.txt')
+brain_data = read.table('~/data/dti/nvVSpersistent_FA_skeletonised.txt')
 out = brain_data[,1:4]
+
+# tmp = gf$SX_inattb
+# data = gf$SX_inattb
+data = gf$DX_GROUP
+nsubj = dim(gf)[1]
+tmp = vector()
+for (i in 1:nsubj) {
+#     if (gf[i,]$DX_GROUP=='persistent') {
+    if (gf[i,]$DX_GROUP=='NV') {
+        tmp = c(tmp, data[i])
+    }
+}
+for (i in 1:nsubj) {
+#     if (gf[i,]$DX_GROUP=='remitted') {
+    if (gf[i,]$DX_GROUP=='persistent') {
+        tmp = c(tmp, data[i])
+    }
+}
+tmp = as.factor(tmp)
+# tmp = -1*tmp
+
 data = as.matrix(brain_data[,4:dim(brain_data)[2]])
 nvoxels = dim(data)[1]
 stats = vector(length=nvoxels, mode="numeric")
-for (v in 1:dim(data)[1]) {
-    fit = lm(data[v,]~tmp)
+# run the model for the actual data
+for (v in 1:nvoxels) {
+#     fit = lm(data[v,]~tmp)
     # CHECK THIS FOR EVERY MODEL!
-    stats[v] = summary(fit)$coefficients[2,3]
+#     stats[v] = summary(fit)$coefficients[2,3]
+    stats[v] = t.test(data[v,] ~ tmp, alternative="greater")$statistic
+#     stats[v] = cor.test(data[v,], tmp)$statistic
 }
+# save the statistics to run through TFCE
 out[,4] = stats
-fname = sprintf('%s/inatt_with_NVs', out_dir)
+fname = sprintf('%s/pairwise', out_dir)
+# write the t-stats and convert to nifti
 write.table(out, file=sprintf('%s.txt',fname), col.names=F, row.names=F)
 system(sprintf('cat %s.txt | 3dUndump -master %s/%s -datum float -prefix %s.nii.gz -overwrite -',
                fname, data_dir, nii_template, fname), wait=T)
+# run TFCE in the abs(tstats) and convert back to .txt
 system(sprintf('fslmaths %s.nii.gz -tfce 2 1 26 %s_tfce.nii.gz', fname, fname), wait=T)
 system(sprintf('3dmaskdump -o %s_tfce.txt -overwrite -mask %s/mean_FA_skeleton_mask.nii.gz %s_tfce.nii.gz',
                fname, data_dir, fname), wait=T)
