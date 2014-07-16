ef99ce4838d95576c912d308adde439619e1e443
Gustavo Sudre <gsudre@pobox.com>
2014-07-16 14:31:45 -0400

    TARing files before copying both ways. Piping didnt work because of stdin, but this is still faster

--- lab_mgmt/retrieve_dtiprep_biowulf.sh
+++ lab_mgmt/retrieve_dtiprep_biowulf.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
 maskids=$1
-
-# adding ssh-key to shell (needed for sbrbmeg)
-exec /usr/bin/ssh-agent $SHELL
-ssh-add ~/.ssh/id_rsa_helix
+start_dir=`pwd`
 
 # for each mask id in the file
 while read m; do 
     echo 'Working on maskid' $m
 
     # saving original edti_proc folder
-    cp -rv /mnt/neuro/data_by_maskID/${m}/edti_proc /mnt/neuro/data_by_maskID/${m}/edti_proc_beforeDIFFPREP
+    if [ ! -d /mnt/neuro/data_by_maskID/${m}/edti_proc_beforeDIFFPREP ]; then
+        cp -rv /mnt/neuro/data_by_maskID/${m}/edti_proc /mnt/neuro/data_by_maskID/${m}/edti_proc_beforeDIFFPREP
+    fi
 
     # copying over the generated files
-    scp -r bw:~/data/tortoise/${m}/edti_proc /mnt/neuro/data_by_maskID/${m}/
+    cd /mnt/neuro/data_by_maskID/${m}
+    ssh -nq biowulf.nih.gov "cd ~/data/tortoise/${m}; tar czf tmp.tar.gz edti_proc"
+    scp -q biowulf.nih.gov:~/data/tortoise/${m}/tmp.tar.gz .
+    tar -zxf tmp.tar.gz
+    rm tmp.tar.gz
+    ssh -nq biowulf.nih.gov "rm ~/data/tortoise/${m}/tmp.tar.gz"
+    
 done < $maskids
+
+# check which subjects ran all the way to the end
+while read m; do 
+    ls /mnt/neuro/data_by_maskID/${m}/edti_proc/*DMC.list
+done < $maskids
+
+cd $start_dir
--- lab_mgmt/setup_dtiprep_biowulf.sh
+++ lab_mgmt/setup_dtiprep_biowulf.sh
@@ -2,25 +2,26 @@
 
 maskids=$1
 batchFile=~/tortoise_in_biowulf/tortoise.bat
+start_dir=`pwd`
 
 cp ~/tortoise_in_biowulf/tortoise_template.bat ${batchFile}
 suffix=''
 # for each mask id in the file
 while read m; do 
-    echo 'Working on maskid' $m
-
-    # copying over the necessary files
-    mkdir /tmp/${m}
-    scp -r /tmp/${m} bw:~/data/tortoise/
-    scp -r /mnt/neuro/data_by_maskID/${m}/edti/ bw:~/data/tortoise/${m}/
-    scp -r /mnt/neuro/data_by_maskID/${m}/edti_proc/ bw:~/data/tortoise/${m}/
-    scp /mnt/neuro/data_by_maskID/${m}/edti/t2_struc_acpc.nii bw:~/data/tortoise/${m}/
-    rmdir /tmp/${m}
+    echo "Copying over eDTI files for ${m}"
+    # # copying over the necessary files
+    ssh -nq biowulf.nih.gov "mkdir ~/data/tortoise/${m}"
+    cd /mnt/neuro/data_by_maskID/${m}
+    # piping is not really working, so I'll just tar and copy it over. It'll will still be faster, I hope
+    tar czf tmp.tar.gz edti edti_proc
+    scp -q tmp.tar.gz biowulf.nih.gov:~/data/tortoise/${m}/
+    ssh -nq biowulf.nih.gov "cd ~/data/tortoise/${m}; tar xzf tmp.tar.gz; rm tmp.tar.gz"
+    rm tmp.tar.gz
 
     # setting up XML file
     cp ~/tortoise_in_biowulf/0000_template.xml ~/tortoise_in_biowulf/${m}.xml
     perl -p -i -e "s/0000/${m}/g" ~/tortoise_in_biowulf/${m}.xml
-    scp ~/tortoise_in_biowulf/${m}.xml bw:~/tortoiseXMLfiles/
+    scp -q ~/tortoise_in_biowulf/${m}.xml bw:~/tortoiseXMLfiles/
     rm ~/tortoise_in_biowulf/${m}.xml
 
     # adding mask id to the current batch file
@@ -32,4 +33,6 @@ done < $maskids
 
 echo "wait" >> ${batchFile}
 mv ${batchFile} ~/tortoise_in_biowulf/tortoise${suffix}.bat
-scp ~/tortoise_in_biowulf/tortoise${suffix}.bat bw:~/scripts/
+scp -q ~/tortoise_in_biowulf/tortoise${suffix}.bat bw:~/scripts/
+echo "Batch name: tortoise${suffix}.bat"
+cd $start_dir
