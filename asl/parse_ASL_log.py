50773e47f9003e7cf36f204f5e214285f14d33b8
Gustavo Sudre <gsudre@pobox.com>
2014-09-05 15:47:00 -0400

    Updated functions to conform to new analysis parameters

--- asl/parse_ASL_log.py
+++ asl/parse_ASL_log.py
@@ -3,65 +3,83 @@ import re
 import operator
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+home = os.path.expanduser('~')
 
-fname = '/Users/sudregp/data/results/asl/log_1317_2.txt'
-title_str = '1317 resting'
+maskid = '1362'
+log_dir = home + '/data/results/asl/'
+nvols = 23
 
-thresh = []
-outliers = []
-label_data = {}
-fid = open(fname,'r')
-waiting_for_value = False
-for line in fid:
-    if waiting_for_value:
-        waiting_for_value = False
-        obj = re.search('^(\S+)', line)
-        cbf = float(obj.group(1))
-        if label_name in label_data.keys():
-            label_data[label_name].append(cbf)
-        else:
-            label_data[label_name] = [cbf]
-    else:
-        obj = re.search('^Outlier detection threshold = (.+)*', line)
-        if obj is not None:
-            thresh.append(float(obj.group(1)))
-        obj = re.search('^Num. outlier volumes = (\d+)*, Outlier', line)
-        if obj is not None:
-            outliers.append(100*(1-float(obj.group(1))/23))
-        obj = re.search('^::(\w+):(\w+)::', line)
-        if obj is not None:
-            label_name = obj.group(1) + '-' + obj.group(2)
-            waiting_for_value = True
-        
-# organizing label data into left and right matrices
-# transforms the dictionary into a list of tuples sorted by key
-label_data = sorted(label_data.iteritems(), key=operator.itemgetter(0))
-num_points = len(label_data[0][1])
-num_labels = len(label_data)/2
-x = np.array(thresh)
-plt.figure(figsize=(10.2, 8.5))
+# the outer loop is based on the figure layout
+plt.figure(figsize=(10, 12))
 plt_cnt = 1
-for h in ('left','right'):
-    y = np.empty([num_points, num_labels])
-    cnt = 0
-    names = []
-    for key, vals in label_data:
-        if key.find(h)==0:
-            y[:, cnt] = np.array(vals)
-            names.append(key.replace(h+'-',''))
-            cnt += 1
-    plt.subplot(3,1,plt_cnt)
-    plt.plot(x, y)
-    plt.plot(x, outliers, 'ko:')
-    plt.title('%s %s'%(h, title_str))
-    plt_cnt += 1
-plt.xlabel('Threshold (fraction of outlier pixels)')
-plt.ylabel('CBF')
-plt.subplot(3,1,3)
-plt.plot(x, y)
-plt.legend(names,loc='center',ncol=2)
-plt.show()
+task_outliers = []
+for task in ['vid1','vid2','tfree1','tfree2']:
+    fname = log_dir + '/log_%s_%s.txt'%(maskid,task) 
+    thresh = []
+    outliers = []
+    label_data = {}
+    fid = open(fname,'r')
+    waiting_for_value = False
+    label_name = 'no_label'
+    for line in fid:
+        if waiting_for_value:
+            waiting_for_value = False
+            obj = re.search('^(\S+)', line)
+            cbf = float(obj.group(1))
+            if label_name in label_data.keys():
+                label_data[label_name].append(cbf)
+            else:
+                label_data[label_name] = [cbf]
+        else:
+            obj = re.search('^Outlier detection threshold = (.+)*', line)
+            if obj is not None:
+                thresh.append(float(obj.group(1)))
+            obj = re.search('^Num. outlier volumes = (\d+)*, Outlier', line)
+            if obj is not None:
+                outliers.append(100*(1-float(obj.group(1))/nvols))
+            obj = re.search('^::(\w+):(\w+)::', line)
+            if obj is not None:
+                label_name = obj.group(1) + '-' + obj.group(2)
+                waiting_for_value = True
+            
+    # organizing label data into left and right matrices
+    # transforms the dictionary into a list of tuples sorted by key
+    label_data = sorted(label_data.iteritems(), key=operator.itemgetter(0))
+    num_points = len(label_data[0][1])
+    num_labels = len(label_data)/2
+    x = np.array(thresh)
+    for h in ('left','right'):
+        plt.subplot(5,2,plt_cnt)
+        y = np.empty([num_points, num_labels])
+        cnt = 0
+        names = []
+        for key, vals in label_data:
+            if key.find(h)==0:
+                y[:, cnt] = np.array(vals)
+                names.append(key.replace(h+'-',''))
+                cnt += 1
+        
+        plt.plot(x, y)
+        plt.title('%s: %s (%s)'%(maskid, task, h))
+        plt.xticks([])
+        if plt_cnt==1:
+            plt.ylabel('CBF')
+        else:
+            plt.ylabel('')
+        plt_cnt += 1
+    task_outliers.append(outliers)
 
-'''
-voxels included
-'''
+# plotting the bottom two subplots
+plt.subplot(5,2,plt_cnt)
+y2 = np.array(task_outliers).T
+plt.plot(x, y2)
+plt.ylabel('Pct of good volumes')
+plt.xlabel('Threshold')
+plt.title('Time points (data) used')
+
+plt.subplot(5,2,plt_cnt+1)
+plt.plot(x, y)
+plt.legend(names,loc='center',ncol=2,fontsize='x-small')
+
+plt.show()
--- asl/run_multiple_procPCASL.sh
+++ asl/run_multiple_procPCASL.sh
@@ -1,18 +1,19 @@
-fnum=2
-logfile=/Users/sudregp/data/results/asl/log_1317_${fnum}.txt
-#thresh=(.01 .015 .02 .025 .05 .075 .1 .125 .15 .175 .2 .225 .25 .275 .3)
-thresh=(.02 .03 .04 .05 .06 .35 .46 .5)
+maskid=1362
+thresh=(.01 .015 .02 .025 .05 .075 .1 .125 .15 .175 .2 .225 .25 .275 .3)
 labels=(Caudate Putamen Thalamus Superior_Temporal_Gyrus Inferior_Parietal_Lobule Medial_Globus_Pallidus Lateral_Globus_Pallidus Medial_Frontal_Gyrus Superior_Occipital_Gyrus)
 
-echo ================= > $logfile
-for t in ${thresh[@]}; do
-    csh /Users/sudregp/research_code/asl/procPCASL.sh volreg${fnum}+orig.BRIK 2500 1500 redo $t | tee -a $logfile
-    3dAllineate -base mprage+tlrc -input volreg${fnum}_cbf+orig -1Dmatrix_apply mat.${fnum}.warp.aff12.1D -prefix volreg${fnum}_cbf -overwrite
-    for h in left right; do
-        for l in ${labels[@]}; do
-            echo ::${h}:${l}:: >> $logfile
-            3dmaskave -mask TT_Daemon:${h}:${l} volreg${fnum}_cbf+tlrc | tee -a $logfile
+for scan in vid1 vid2 tfree1 tfree2; do
+    logfile=/Users/sudregp/data/results/asl/log_${maskid}_${scan}.txt
+    echo ================= > $logfile
+    for t in ${thresh[@]}; do
+        csh /Users/sudregp/research_code/asl/procPCASL.sh volreg.${scan}+orig.BRIK 2500 1500 redo $t | tee -a $logfile
+        3dAllineate -base anat+tlrc -input volreg.${scan}_cbf+orig -1Dmatrix_apply volreg.${scan}_al_tlrc_mat.aff12.1D -prefix volreg.${scan}_cbf+tlrc -overwrite
+        for h in left right; do
+            for l in ${labels[@]}; do
+                echo ::${h}:${l}:: >> $logfile
+                3dmaskave -mask TT_Daemon:${h}:${l} volreg.${scan}_cbf+tlrc | tee -a $logfile
+            done
         done
+        echo ================= >> $logfile
     done
-    echo ================= >> $logfile
 done
