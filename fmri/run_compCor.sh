45db19226fca8e27fe9c7d18f7c18395a021590c
Gustavo Sudre <gsudre@pobox.com>
2014-09-19 17:08:47 -0400

    Created an afni_proc just for compCor, and the rest of the script to run the regression

--- fmri/run_compCor.sh
+++ fmri/run_compCor.sh
@@ -1,22 +1,48 @@
 #!/bin/sh
 # Runs the rest of the compCor pipeline
+# Note that all these computations need to be performed in +orig because we have to keep the aparc and binary values in masks. We convert it to TLRC in the end
 
 subj=$1
-subj_dir=/mnt/neuro/data_by_maskID/$subj/afni
+subj_dir=/mnt/neuro/data_by_maskID/$subj/afni/${subj}.rest.compCor.results
+freesurfer_dir=/mnt/neuro/freesurfer_subjects/$subj
 old_pwd=`pwd`
 # only go through if there is a compCor run
-if [ -d ${subj_dir}/${subj}.rest.compCor.results ]; then
-    cd ${subj_dir}/${subj}.rest.compCor.results
-    # create WM + CSF mask
-    3dcalc -a mask_WMe_resam+tlrc -b mask_CSFe_resam+tlrc -expr 'or(a,b)' -prefix mask_WMeORCSFe_resam+tlrc
-    # select out censored TRs
-    keep_trs=`1d_tool.py -infile X.xmat.1D -show_trs_uncensored encoded`
-    # detrend the signal prior to blurring
-    3dDetrend -prefix pb03.${subj}.r01.volreg.detrend -polort 3 pb03.${subj}.r01.volreg+tlrc"[$keep_trs]"
-    # calculate the first 3 PCs
-    3dpc -pcsave 3 -mask mask_WMeORCSFe_resam+tlrc -prefix ${subj}.WMCSFePC pb03.${subj}.r01.volreg.detrend+tlrc
-    # regress out old X matrix (includes CSFe and motion), plus PCs and WM
-    3dTfitter -polort -1 -RHS all_runs.$subj+tlrc"[$keep_trs]" -LHS X.xmat.1D WMeLocal_rall+tlrc ${subj}.WMCSFePC.1D -prefix stats.compCor.$subj -fitts fitts.compCor.$subj -errsum errts.compCor.$subj
+if [ -d $subj_dir ]; then
+    cd $subj_dir
+    if [ -d $freesurfer_dir ]; then
+        # create the CSF mask using Freesurfer
+        mri_convert ${freesurfer_dir}/mri/aparc+aseg.mgz anat_aparc+orig.nii.gz
+        3dresample -inset anat_aparc+orig.nii.gz -prefix anat_aparc.resample+orig -master pb03.${subj}.r01.volreg+orig -rmode NN 
+        # extract ventricles mask
+        3dcalc -a anat_aparc.resample+orig -expr 'equals(a,4)+equals(a,43)' -prefix mask_ventricles
+        # extract caudate, accumbens, and thalamic mask and dilate it
+        3dcalc -a anat_aparc.resample+orig -expr 'equals(a,10)+equals(a,11)+equals(a,26)+equals(a,49)+equals(a,50)+equals(a,58)' -prefix mask_nonventricles 
+        3dcalc -a mask_nonventricles+orig -b a+i -c a-i -d a+j -e a-j -f a+k -g a-k -expr 'amongst(1,a,b,c,d,e,f,g)' -prefix mask_nonventricles.dilated
+        # make sure there is no GM in CSF mask
+        3dcalc -a mask_ventricles+orig -b mask_nonventricles.dilated+orig -expr 'a-step(a*b)' -prefix mask_ventricles.eroded  
+
+        # compute 3 PCs from nuisance mask (CSF+WM) and create regressor
+        3dcalc -a mask_WMe_resam+orig -b mask_ventricles.eroded+orig -expr 'or(a,b)' -prefix mask_nuisance
+        # select out censored TRs
+        keep_trs=`1d_tool.py -infile X.xmat.1D -show_trs_uncensored encoded`
+        # detrend the signal prior to blurring, only using the good TRs
+        3dDetrend -prefix pb03.${subj}.r01.volreg.detrend -polort 3 pb03.${subj}.r01.volreg+orig"[$keep_trs]"
+        # calculate the first 3 PCs
+        3dpc -pcsave 3 -mask mask_nuisance+orig -prefix ${subj}.nuisancePC pb03.${subj}.r01.volreg.detrend+orig
+
+        # get mean CSF signal regressor
+        3dmaskave -q -mask mask_ventricles.eroded+orig pb03.${subj}.r01.volreg+orig"[$keep_trs]" > mean_CSF.1D
+        # get mean WM signal regressor (using 3dSeg mask)
+        3dmaskave -q -mask mask_WMe_resam+orig pb03.${subj}.r01.volreg+orig"[$keep_trs]" > mean_WM.1D
+
+        # regress out old X matrix (includes motion parameters), plus PCs, WM, and CSF
+        3dTproject -polort 0 -input all_runs.$subj+orig"[$keep_trs]" -ort X.xmat.1D -ort ${subj}.nuisancePC_vec.1D -ort mean_WM.1D -ort mean_CSF.1D -prefix errts.${subj}.compCor
+        # Take final result to TLRC space
+        cat_matvec -ONELINE mprage_ns+tlrc::WARP_DATA -I mprage_al_junk_mat.aff12.1D -I mat.r01.vr.aff12.1D > mat.epi2tlrc.1D
+        3dAllineate -base mprage_ns+tlrc -input errts.${subj}.compCor+orig -1Dmatrix_apply mat.epi2tlrc.1D -mast_dxyz 2.5 -prefix errts.${subj}.compCor
+    else
+        echo $subj has no Freesurfer run
+    fi    
 else
     echo $subj has no compCor directory
 fi
---
+++ fmri/run_resting_afni_proc_compCorOnly.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+subj=$1
+
+subj_dir=/mnt/neuro/data_by_maskID/$subj/afni
+
+# This one is based on Joel's latest paipeline (see his e-mail). Afterwards, we still need to regress out first 3 PCs of a combined CSFe/WMe mask (CompCor)
+afni_proc.py -subj_id $subj                               \
+    -dsets ${subj_dir}/rest+orig.HEAD                          \
+    -script $subj_dir/rest.proc.compCor.$subj               \
+    -copy_anat $subj_dir/mprage+orig       \
+    -out_dir $subj_dir/$subj.rest.compCor.results               \
+    -blocks despike tshift align volreg blur mask regress tlrc \
+    -tcat_remove_first_trs 3                \
+    -volreg_align_e2a                                   \
+    -mask_segment_anat yes                              \
+    -regress_censor_motion 0.2                          \
+    -regress_censor_outliers 0.1                        \
+    -regress_apply_mot_types demean deriv               \
+    -regress_anaticor                                   \
+    -regress_anaticor_radius 25                         \
+    -regress_run_clustsim yes                            \
+    -regress_est_blur_errts                                
+tcsh -xef $subj_dir/rest.proc.compCor.$subj | tee $subj_dir/output.rest.compCor.proc.$subj
