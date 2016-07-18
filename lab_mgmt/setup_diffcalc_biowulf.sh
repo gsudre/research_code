ed0787fc706c77dbe1f785e796fb269fa72cd56a
Gustavo Sudre <gustavo.sudre@nih.gov>
2016-07-18 10:54:05 -0400

    changed biowulf name

--- lab_mgmt/setup_diffcalc_biowulf.sh
+++ lab_mgmt/setup_diffcalc_biowulf.sh
@@ -14,7 +14,7 @@ while read m; do
     echo "ssh -q biowulf.nih.gov \"mkdir /scratch/ncr/${m}\"" >> $tmp_script
     echo "ssh -q biowulf.nih.gov \"mkdir /scratch/ncr/${m}/edti_proc\"" >> $tmp_script
     echo "cd /mnt/shaw/data_by_maskID/${m}/edti_proc" >> $tmp_script 
-    echo "tar czf - edti_DMC* | ssh -q biowulf2.nih.gov \"cd /scratch/ncr/${m}/edti_proc/; tar xzf -\"" >> $tmp_script
+    echo "tar czf - edti_DMC* | ssh -q biowulf.nih.gov \"cd /scratch/ncr/${m}/edti_proc/; tar xzf -\"" >> $tmp_script
 
 done < $maskids
 
