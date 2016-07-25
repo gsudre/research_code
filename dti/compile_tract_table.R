23c313fca105a0d3d2baf2f8af465ade878692bf
Gustavo Sudre <gustavo.sudregp@nih.gov>
2016-07-25 08:53:20 -0400

    changing paths

--- dti/compile_tract_table.R
+++ dti/compile_tract_table.R
@@ -2,8 +2,8 @@
 tract_names = c('left_cst', 'left_ifo', 'left_ilf', 'left_slf', 'left_unc', 'right_cst', 'right_ifo', 'right_ilf', 'right_slf', 'right_unc', 'cc')
 var_names = c('FA', 'AD', 'RD')#, 'MO')
 
-subj_file = '~/tmp/dti/subjs_diffeo.txt'
-data_dir = '~/tmp/dti/'
+subj_file = '/mnt/shaw/dti_robust_tsa/analysis_99redo/subjs_diffeo.txt'
+data_dir = '/mnt/shaw/dti_robust_tsa/analysis_99redo/'
 in_fname = 'mean_sampling.gzip'
 out_fname = 'dti_max_phenotype_cleanedWithinTract3sd.csv'
 out_fname = 'dti_mean_phenotype.csv'
