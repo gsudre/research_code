59ea0841160af7990bf4ec555acd1d7913c59b5a
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-03-29 15:23:06 -0400

    function to assess impact of QC

---
+++ qc_impact_loop.R
@@ -0,0 +1,44 @@
+# Checks the impact of QC in different brain metrics
+
+df = read.csv('/Volumes/Shaw/longitudinal_anatomy/all_data_philip_update2_gs2.csv')
+
+# we need to manually transform some metrics so that smaller is always better
+df$mriqc_qi_2 = -log10(df$mriqc_qi_2)
+df$mriqc_fber = -log10(df$mriqc_fber)
+df$mriqc_cnr = -df$mriqc_cnr
+df$mriqc_snr_total = -df$mriqc_snr_total
+
+# qc metrics to use
+qc_metrics = c('mriqc_qi_2', 'mriqc_fber', 'mriqc_cnr', 'mriqc_snr_total', 'mriqc_fwhm_avg', 'mriqc_efc',
+               'civetqc_MASK_ERROR', 'civetqc_LEFT_INTER', 'civetqc_RIGHT_INTER')
+
+# breaks in the data, where smaller is better, 0 to 100 (e.g. 10 = use top 10% of the data with highest quality)
+qbreaks = c(10, 25, 50, 75, 100)
+
+brain_vars = c('freesurfer_crossPipe_Brain.Stem', 'freesurfer_crossPipe_lh_caudalmiddlefrontal_area')
+# brain_vars = colnames(df)[grepl('^freesurfer_crossPipe_', colnames(df))]
+
+for (roi in brain_vars) {
+  print(roi)
+  # results will be qbreaks by metric*3
+  res = as.data.frame(matrix(nrow = length(qbreaks), ncol = length(qc_metrics)*3),
+                      row.names = sapply(qbreaks, function(x) sprintf('q%d', x)))
+  colnames(res) = unlist(lapply(qc_metrics, function(x) c(sprintf('%s_n', x),
+                                                          sprintf('%s_tstat', x),
+                                                          sprintf('%s_pval', x))))
+  
+  for (i in 1:length(qbreaks)) {
+    for (qc in qc_metrics) {
+      thresh = quantile(df[, qc], probs=qbreaks[i]/100, na.rm=T)
+      idx = which(df[, qc] <= thresh)  # just to deal with NAs
+      # use covariates always in the end!!!
+      fm_str = '%s ~ %s + AGESCAN + SEX'
+      fit = lm(as.formula(sprintf(fm_str, roi, qc)), data=df[idx, ])
+      # collecting results
+      res[i, sprintf('%s_n', qc)] = length(idx)
+      res[i, sprintf('%s_tstat', qc)] = summary(fit)$coefficients[qc, 't value']
+      res[i, sprintf('%s_pval', qc)] = summary(fit)$coefficients[qc, 'Pr(>|t|)']
+    }
+  }
+  write.csv(res, file=sprintf('~/tmp/qc_impact_%s.csv', roi))
+}
