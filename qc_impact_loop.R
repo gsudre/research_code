8c387bcf443283697209f7545cba40b33582956e
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-03-30 09:11:03 -0400

    modified version to output all brain regions to the same table

--- qc_impact_loop.R
+++ qc_impact_loop.R
@@ -19,16 +19,18 @@ qbreaks = c(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 9
 # brain_vars = c('freesurfer_crossPipe_Brain.Stem', 'freesurfer_crossPipe_lh_caudalmiddlefrontal_area')
 brain_vars = colnames(df)[grepl('^freesurfer_crossPipe_', colnames(df))]
 
+all_res = c()
 for (roi in brain_vars) {
   print(roi)
-  # results will be qbreaks by metric*3
-  res = as.data.frame(matrix(nrow = length(qbreaks), ncol = length(qc_metrics)*3),
-                      row.names = sapply(qbreaks, function(x) sprintf('q%d', x)))
-  colnames(res) = unlist(lapply(qc_metrics, function(x) c(sprintf('%s_n', x),
+  # results will be qbreaks by metric*3 + 2 (for roi and Q)
+  res = as.data.frame(matrix(nrow = length(qbreaks), ncol = (length(qc_metrics)*3 + 2)))
+  colnames(res) = c('roi', 'quantile', unlist(lapply(qc_metrics, function(x) c(sprintf('%s_n', x),
                                                           sprintf('%s_tstat', x),
-                                                          sprintf('%s_pval', x))))
+                                                          sprintf('%s_pval', x)))))
   
   for (i in 1:length(qbreaks)) {
+    res[i, 'quantile'] = qbreaks[i]
+    res[i, 'roi'] = gsub('freesurfer_crossPipe_', '', roi)
     for (qc in qc_metrics) {
       thresh = quantile(df[, qc], probs=qbreaks[i]/100, na.rm=T)
       idx = which(df[, qc] <= thresh)  # just to deal with NAs
@@ -43,5 +45,6 @@ for (roi in brain_vars) {
       }
     }
   }
-  write.csv(res, file=sprintf('~/tmp/qc_impact_%s.csv', roi))
+  all_res = rbind(all_res, res)
 }
+write.csv(all_res, file='~/tmp/qc_impact.csv', row.names = F)
