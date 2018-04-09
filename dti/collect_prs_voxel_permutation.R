839c8e515209d5fadab0f5d0b7527c69322f4658
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-04-09 10:16:14 -0400

    collecting voxelwise dti prs

--- dti/collect_prs_voxel_permutation.R
+++ dti/collect_prs_voxel_permutation.R
@@ -1,19 +1,18 @@
-p = read.table('/scratch/sudregp/1080_rd.txt')
+p = read.table('/scratch/sudregp/1080_rd.txt')  # just to get voxel coordinates
+nvox = nrow(p)
 b=p
-data_dir = '/scratch/sudregp/prs/dti_voxels_fa_wnhaa_extendedfamID_lme_1kg9_cov_agePlusSex/'
-profiles = list.files(path=data_dir, pattern="^PROFILES*", include.dirs=T)
-for (prof in profiles) {
-  Ys = list.files(path=sprintf('%s/%s/', data_dir, prof), pattern="*", include.dirs=T)
-  for (y in Ys) {
-    print(c(prof, y))
-    for (i in 1:12014) {
+data_dir = '/scratch/sudregp/prs/dti_voxels_fa_wnhaa_extendedfamID_lme_1kg9_cov_agePlusSex/PROFILES.0.1.profile/SX_HI/'
+perms = list.files(path=data_dir, pattern="^perm*", include.dirs=T)
+for (perm in perms) {
+    print(perm)
+    for (i in 1:nvox) {
       if (i %% 1000 == 0) {print(i)}
-      fname = sprintf('%s/%s/%s/v%05d.csv', data_dir, prof, y, i)
+      fname = sprintf('%s/%s/v%05d.csv', data_dir, perm, i)
       d = read.csv(fname)
       p[i, 4] = 1-d$acme_p
       b[i, 4] = d$acme
     }
-    write.table(p, file=sprintf('%s/%s_%s_pvals.txt', data_dir, prof, y), row.names=F, col.names=F)
-    write.table(b, file=sprintf('%s/%s_%s_betas.txt', data_dir, prof, y), row.names=F, col.names=F)
+    write.table(p, file=sprintf('%s/pvals_%s.txt', data_dir, prof, y), row.names=F, col.names=F)
+    write.table(b, file=sprintf('%s/betas_%s.txt', data_dir, prof, y), row.names=F, col.names=F)
   }
 }
