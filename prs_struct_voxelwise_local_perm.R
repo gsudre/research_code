da4a9acd6c309fdf7b98b26710f19880b72bf99b
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-04-27 11:42:20 -0400

    struct perm scripts

---
+++ prs_struct_voxelwise_local_perm.R
@@ -0,0 +1,131 @@
+# runs mediation for each voxel individually
+args <- commandArgs(trailingOnly = TRUE)
+m_file = args[1]
+local = args[2]
+hemi = args[3]
+myseed = args[4]
+x_str = args[5]
+y_str = args[6]
+
+if (local=='lscratch') {
+  jobid=Sys.getenv('SLURM_JOBID')
+  local=sprintf('/lscratch/%s/', jobid)
+}
+
+gf_fname = sprintf('%s/struct_updated_clin_and_vol_278_04192018.csv', local)
+maskid_fname = sprintf('%s/maskids_struct_updated_clin_and_vol_278_04192018_3tonly.txt', local)
+voxel_fname = sprintf('%s/%s.volume.10.gzip', local, hemi)
+dir_root = sprintf('%s/struct_voxels_volume_%s_273_wnhaa_extendedfamID_lme_1kg9_cov_agePlusSex', local, hemi)
+
+mydata<-read.csv(gf_fname)
+
+load(voxel_fname)
+
+cnames = sapply(1:163842, function(x) sprintf('v%06d', x))
+colnames(data) = cnames
+
+maskid = read.table(maskid_fname)[,1]
+data = cbind(maskid, data)
+
+
+dim(mydata)
+dim(data)
+mydata = merge(mydata, data, by='maskid')
+mydata$SX_TOTAL = mydata$SX_INATT + mydata$SX_HI
+dim(mydata)
+
+# choosing mediators
+
+nboot = 2
+mixed = T
+
+# no need to change anything below here. The functions remove NAs and zscore variables on their own
+run_model4 = function(X, M, Y, nboot=1000, short=T, data2) {
+  library(mediation)
+  idx = is.na(X) | is.na(Y) | is.na(M)
+  Y = Y[!idx]
+  imdiscrete = T
+  if (!is.factor(Y)) {
+    Y = scale(Y)
+    imdiscrete = F
+  }
+  run_data = data.frame(X = scale(X[!idx]),
+                        Y = Y,
+                        M = scale(M[!idx]),
+                        FAMID = data2[!idx,]$extendedFamID,
+                        age= data2[!idx,]$AGE_CLIN,
+                        sex = data2[!idx,]$Sex)
+  
+  if (!is.na(run_data[1,]$FAMID)) {
+    library(lme4)
+    fm = as.formula('M ~ X + age + sex + (1|FAMID)')
+    fy = as.formula('Y ~ X + M + age + sex + (1|FAMID)')
+    model.M <- lmer(fm, data=run_data)
+    if (imdiscrete) {
+      model.Y <- glmer(fy, data=run_data, family=binomial(link='logit'))
+    } else {
+      model.Y <- lmer(fy, data=run_data)
+    }
+    results <- mediate(model.M, model.Y, treat='X', mediator='M', boot=F, sims=nboot)
+  } else {
+    fm = as.formula('M ~ X + age + sex')
+    fy = as.formula('Y ~ X + M + age + sex')
+    model.M <- lm(fm, data=run_data)
+    if (imdiscrete) {
+      model.Y <- glm(fy, data=run_data, family=binomial(link='logit'))
+    } else {
+      model.Y <- lm(fy, data=run_data)
+    }
+    results <- mediate(model.M, model.Y, treat='X', mediator='M', boot=T, sims=nboot, boot.ci.type='bca')
+  }
+  
+  if (short) {
+res = c(results$nobs, results$tau.coef, results$tau.p, results$d.avg, results$d.avg.p,
+            results$z.avg, results$z.avg.p, results$n.avg, results$n.avg.p, results$tau.ci[1], results$tau.ci[2],
+            results$d.avg.ci[1], results$d.avg.ci[2], results$z.avg.ci[1], results$z.avg.ci[2], results$n.avg.ci[1], results$n.avg.ci[2])
+    names(res) = c('nobs', 'tot', 'tot_p', 'acme', 'acme_p', 'ade', 'ade_p', 'prop', 'prop_p',
+                   'tot_2p5ci', 'tot_97p5ci', 'acme_2p5ci', 'acme_97p5ci', 'ade_2p5ci', 'ade_97p5ci', 'prop_2p5ci', 'prop_97p5ci')
+    return(res)     
+  } else {
+    return(results)
+  }
+}
+
+if (!mixed) {
+  mydata$NuclearFamID = NA
+}
+
+dir.create(dir_root, showWarnings = FALSE)
+dir_name = sprintf('%s/%s', dir_root, x_str)
+dir.create(dir_name, showWarnings = FALSE)
+X = mydata[, x_str]
+dir_name = sprintf('%s/%s/%s', dir_root, x_str, y_str)
+dir.create(dir_name, showWarnings = FALSE)
+Y = mydata[, y_str]
+dir_name = sprintf('%s/%s/%s/perm%s', dir_root, x_str, y_str, myseed)
+dir.create(dir_name, showWarnings = FALSE)
+
+# shuffle mydata around, but only change M, as we want the relationship between
+# X and Y to stay the same
+set.seed(as.integer(myseed) %% 2^31)
+perm_labels <- sample.int(dim(mydata)[1], replace = FALSE)
+print(perm_labels[1:10])
+
+vox_list = read.table(m_file)[, 1]
+
+X = mydata[, x_str]
+Y = mydata[, y_str]
+for (m_str in vox_list) {
+    out_fname = sprintf('%s/%s/%s/%s.csv', dir_root, x_str, y_str, m_str)
+    print(out_fname)
+    # some voxels are all zeros! give them a bad result
+    if (sum(mydata[, m_str]==0) == nrow(mydata)) {
+        res_names = c('nobs', 'tot', 'tot_p', 'acme', 'acme_p', 'ade', 'ade_p', 'prop', 'prop_p',
+                    'tot_2p5ci', 'tot_97p5ci', 'acme_2p5ci', 'acme_97p5ci', 'ade_2p5ci', 'ade_97p5ci', 'prop_2p5ci', 'prop_97p5ci')
+        all_res = rep(1, length(res_names))
+        names(all_res) = res_names    
+    } else {
+        all_res = run_model4(X, mydata[, m_str], Y, nboot=nboot, data2=mydata)
+    }
+    write.csv(t(all_res), file=out_fname, row.names=F, quote=F)
+}
---
+++ prs_struct_wrap_local_perms.sh
@@ -0,0 +1,21 @@
+# runs one permutation of PRS voxelwise strucutal locally, then copies over compiled files
+hemi=$1
+perm=`printf %04d $2`
+x=$3
+y=$4
+
+module load R
+cd /lscratch/$SLURM_JOBID
+
+# for i in {1..163842}; do v=`printf v%06d $i`; echo $v >> voxel_list.txt; done
+cp /data/${USER}/prs/voxel_list_struct ./voxel_list.txt
+
+cp /scratch/${USER}/prs/struct_updated_clin_and_vol_278_04192018.csv .
+cp /scratch/${USER}/prs/maskids_struct_updated_clin_and_vol_278_04192018_3tonly.txt .
+cp /scratch/${USER}/prs/${hemi}.volume.10.gzip .
+split -l 5121 voxel_list.txt  # adapted to 32 cores!
+
+for f in `ls x*`; do
+    Rscript --vanilla ~/research_code/prs_struct_voxelwise_local_perm.R \
+        /lscratch/${SLURM_JOBID}/$f lscratch ${hemi} $perm $x $y &
+done
