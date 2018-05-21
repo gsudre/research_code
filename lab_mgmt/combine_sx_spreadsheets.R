11403770093c8620a1881cef21383692878e4d99
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-05-21 16:38:37 -0400

    adding family spreadsheet

--- lab_mgmt/combine_sx_spreadsheets.R
+++ lab_mgmt/combine_sx_spreadsheets.R
@@ -1,19 +1,20 @@
+# TODO:
+# - incorporate SX from old papers
+
 library(gdata)
 
 dir_name = '/Volumes/Shaw/Clinical_Interviews/'
-caadid_fname = sprintf('%s/CAADID data 5-9-18.xlsx', dir_name)
-nv_fname = sprintf('%s/nv_interviews_051418.xlsx', dir_name)
-dica_fname = sprintf('%s/DICA 5-8-18.xlsx', dir_name)
+caadid_fname = sprintf('%s/CAADID data 5-21-18.xlsx', dir_name)
+nv_fname = sprintf('%s/nv_interviews_051518.xlsx', dir_name)
+dica_fname = sprintf('%s/DICA 5-21-18.xlsx', dir_name)
 caadidS_fname = sprintf('%s/Simplex/CAADID data simplex.xlsx', dir_name)
 nvS_fname = sprintf('%s/Simplex/nv_interviews_simplex.xlsx', dir_name)
 dicaS_fname = sprintf('%s/Simplex/DICA simplex.xlsx', dir_name)
-# all simplex stuff, old papers
-
-
+family_fname = '/Volumes/Shaw/Family Study List/Family Study List 5-21-18.xlsx'
 
 # cleaning up CAADID
 print(caadid_fname)
-df = read.xls(caadid_fname, sheet = 1, header = TRUE)
+df = read.xls(caadid_fname, sheet = 1, header = TRUE, colClasses='character')
 print(sprintf('Found %d records.', nrow(df)))
 caadid = df[, c('MRN', 'date.collected', 'inatt.as.adult', 'hi.as.adult')]
 colnames(caadid) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
@@ -23,7 +24,7 @@ caadid$other_dx = do.call(paste, df[, other_dx])
 
 # cleaning up NV interviews
 print(nv_fname)
-df = read.xls(nv_fname, sheet = 1, header = TRUE)
+df = read.xls(nv_fname, sheet = 1, header = TRUE, colClasses='character')
 print(sprintf('Found %d records.', nrow(df)))
 nv = df[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
             'inattention.symptoms', 'HI.symptoms')]
@@ -36,7 +37,7 @@ sx = rbind(caadid, nv)
 
 # cleaning up DICA interviews
 print(dica_fname)
-df = read.xls(dica_fname, sheet = 1, header = TRUE)
+df = read.xls(dica_fname, sheet = 1, header = TRUE, colClasses='character')
 print(sprintf('Found %d records.', nrow(df)))
 dica = df[, c('ID', 'Date', 'X..of.inatten', 'X..H.I', 'Comments', 'Meds')]
 # keep all entries off medication
@@ -59,7 +60,7 @@ sx = rbind(sx, dica_clean)
 
 # cleaning up CAADID simplex
 print(caadidS_fname)
-df = read.xls(caadidS_fname, sheet = 1, header = TRUE)
+df = read.xls(caadidS_fname, sheet = 1, header = TRUE, colClasses='character')
 print(sprintf('Found %d records.', nrow(df)))
 caadidS = df[, c('MRN', 'date.collected', 'inatt.as.adult', 'hi.as.adult')]
 colnames(caadidS) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
@@ -70,7 +71,7 @@ sx = rbind(sx, caadidS)
 
 # cleaning up NV interviews simplex
 print(nvS_fname)
-df = read.xls(nvS_fname, sheet = 1, header = TRUE)
+df = read.xls(nvS_fname, sheet = 1, header = TRUE, colClasses='character')
 print(sprintf('Found %d records.', nrow(df)))
 nvS = df[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
             'inattention.symptoms', 'HI.symptoms')]
@@ -82,7 +83,7 @@ sx = rbind(sx, nvS)
 
 # cleaning up DICA interviews simplex
 print(dicaS_fname)
-df = read.xls(dicaS_fname, sheet = 'DICA', header = TRUE)
+df = read.xls(dicaS_fname, sheet = 'DICA', header = TRUE, colClasses='character')
 print(sprintf('Found %d records.', nrow(df)))
 dicaS = df[, c('MR', 'Date', 'X..of.inatten', 'X..H.I', 'Comments', 'Meds')]
 # keep all entries off medication
@@ -103,13 +104,38 @@ print(sprintf('Cleaned to %d records.', nrow(dicaS_clean)))
 colnames(dicaS_clean) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi', 'other_dx', 'source')
 sx = rbind(sx, dicaS_clean)
 
+# cleaning up Family Study list
+print(family_fname)
+df = read.xls(family_fname, sheet = 1, header = TRUE, colClasses='character')  # bypassing some weird error in conversion
+print(sprintf('Found %d records.', nrow(df)))
+family = df[, c('MRN', 'Date.of.Assessment', 'Inatt.sx.adult', 'HI.sx.adult')]
+colnames(family) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
+family$source = 'FamilyStudyList'
+other_dx = c("Other.Dx.1", "Other.Dx.2", "Other.Dx.3")
+family$other_dx = do.call(paste, df[, other_dx])
+# this spreadsheet is special because many entries don't have clinical interviews
+idx = family$DOA=='' | family$SX_inatt=='' | family$SX_hi==''
+family = family[!idx, ]
+sx = rbind(sx, family)
+
+sx_bk = sx
+
+# remove anyone without an MRN
+idx = which(sx$MRN!='')
+sx = sx[idx, ]
+
 # convert the different date formats across sheets
-idx = grepl('-', x)
 x = as.character(sx$DOA)
+idx = grepl('-', x)
 new_date = vector(length=length(sx$DOA))
 new_date[idx] = format(as.Date(x[idx], format='%Y-%m-%d'), format='%m/%d/%Y')
 idx = grepl('/', x)
 new_date[idx] = format(as.Date(x[idx], format='%m/%d/%y'), format='%m/%d/%Y')
 sx$DOA = new_date
-# remove any errors
-sx = sx[which(new_date~=F), ]
+
+# report errors
+idx = which(sx$DOA=='' | sx$DOA==F)
+print(sprintf('%d out of %d with wrong or missing DOA.', length(idx), nrow(sx)))
+
+idx = which(sx$SX_inatt=='' | sx$SX_hi=='')
+print(sprintf('%d out of %d with blank SX.', length(idx), nrow(sx)))
