068927ef9cbc53822bec27403b12176fb9b73f6b
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-06-11 11:55:40 -0400

    imrpvoements in parsing dates and sx

--- lab_mgmt/check_nv_interviews.R
+++ lab_mgmt/check_nv_interviews.R
@@ -38,12 +38,12 @@ for (fname in fnames) {
 }
 
 # based on my tests, the biggest change happened form the 04232018 spreadsheet to 04/24/2018. So, let's see what the major differences are:
-df = read.xls('~/tmp/2018/philip/nv_interviews_042318.xlsx', sheet = 'philip_recorde', header = TRUE, colClasses='character')
+df = read.xls('~/tmp/2015/nv_interviews_042815.xlsx', sheet = 'Sheet1', header = TRUE, colClasses='character')
 nv1 = df[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
              'inattention.symptoms', 'HI.symptoms')]
 colnames(nv1) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
 
-df = read.xls('~/tmp/2018/philip/nv_interviews_060418.xlsx', sheet = 'philip_recode', header = TRUE, colClasses='character')
+df = read.xls('~/tmp/2018/philip/nv_interviews_060618.xlsx', sheet = 'philip_recode', header = TRUE, colClasses='character')
 nv2 = df[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
              'inattention.symptoms', 'HI.symptoms')]
 colnames(nv2) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
@@ -55,3 +55,137 @@ for (r in 1:nrow(nv1)) {
     print(nv1[r,])
   }
 }
+
+
+
+sink('~/Documents/nv_consistency_check_06112018.txt')
+
+# do the same as above, but the seed file keeps changing
+mydir = '~/tmp/2016' #8/philip'
+fnames = dir(path=mydir)
+for (f in 1:(length(fnames)-1)) {
+  seed_fname = sprintf('%s/%s', mydir, fnames[f])
+  fname = sprintf('%s/%s', mydir, fnames[f + 1])
+  
+  print(sprintf('Comparing %s and %s', seed_fname, fname))
+  
+  df_seed = read.xls(seed_fname, sheet = 1, header = TRUE, colClasses='character')
+  df = read.xls(fname, sheet = 1, header = TRUE, colClasses='character')
+  
+  nv1 = df_seed[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
+               'inattention.symptoms', 'HI.symptoms')]
+  nv2 = df[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
+               'inattention.symptoms', 'HI.symptoms')]
+  colnames(nv1) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
+  colnames(nv2) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
+  
+  # let's just reduce the false positives by assuming blanks are zeros
+  nv1[nv1$SX_inatt=='', 'SX_inatt'] = 0
+  nv1[nv1$SX_hi=='', 'SX_hi'] = 0
+  nv2[nv2$SX_inatt=='', 'SX_inatt'] = 0
+  nv2[nv2$SX_hi=='', 'SX_hi'] = 0
+  nv1[nv1$DOA=='', 'DOA'] = 'blank date'
+  nv2[nv2$DOA=='', 'DOA'] = 'blank date'
+  
+  # and convert any dates not formatted like the majority
+  x = nv1$DOA
+  idx = grepl('-', x)
+  new_date = nv1$DOA
+  new_date[idx] = format(as.Date(x[idx], format='%Y-%m-%d'), format='%m/%d/%Y')
+  idx = grepl('/', new_date) & lapply(new_date, nchar) < 10
+  new_date[idx] = format(as.Date(x[idx], format='%m/%d/%y'), format='%m/%d/%Y')
+  # the remaining ones are already %m/%d/%Y
+  nv1$DOA = new_date
+  x = nv2$DOA
+  idx = grepl('-', x)
+  new_date = nv2$DOA
+  new_date[idx] = format(as.Date(x[idx], format='%Y-%m-%d'), format='%m/%d/%Y')
+  idx = grepl('/', new_date) & lapply(new_date, nchar) < 10
+  new_date[idx] = format(as.Date(x[idx], format='%m/%d/%y'), format='%m/%d/%Y')
+  # the remaining ones are already %m/%d/%Y
+  nv2$DOA = new_date
+  
+  
+  # check if all entries in seed file are still there
+  for (r in 1:nrow(nv1)) {
+    idx = nv2$MRN==nv1[r,'MRN'] & nv2$DOA==nv1[r, 'DOA'] & nv2$SX_inatt==nv1[r, 'SX_inatt'] & nv2$SX_hi==nv1[r, 'SX_hi']
+    if (sum(idx)==0) {
+      print('Cannot find:')
+      print(nv1[r,])
+      print('But found:')
+      idx = nv2$MRN==nv1[r,'MRN'] & nv2$DOA==nv1[r, 'DOA']
+      if (sum(idx) > 0) {
+        print(nv2[idx,])
+      } else {
+        idx = nv2$MRN==nv1[r,'MRN']
+        print(nv2[idx,])
+      }
+    }
+  }
+}
+
+
+# same as above, but for 2 files
+fnames = c('~/tmp/2016//nv_interviews_070716.xlsx', '~/tmp/2016//nv_interviews_120816.xlsx')
+for (f in 1:(length(fnames)-1)) {
+  seed_fname = sprintf('%s', fnames[f])
+  fname = sprintf('%s', fnames[f + 1])
+  
+  print(sprintf('Comparing %s and %s', seed_fname, fname))
+  
+  df_seed = read.xls(seed_fname, sheet = 1, header = TRUE, colClasses='character')
+  df = read.xls(fname, sheet = 1, header = TRUE, colClasses='character')
+  
+  nv1 = df_seed[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
+                    'inattention.symptoms', 'HI.symptoms')]
+  nv2 = df[, c('Medical.Record...MRN...Subjects', 'record.date.collected...NV.Interview',
+               'inattention.symptoms', 'HI.symptoms')]
+  colnames(nv1) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
+  colnames(nv2) = c('MRN', 'DOA', 'SX_inatt', 'SX_hi')
+  
+  # let's just reduce the false positives by assuming blanks are zeros
+  nv1[nv1$SX_inatt=='', 'SX_inatt'] = 0
+  nv1[nv1$SX_hi=='', 'SX_hi'] = 0
+  nv2[nv2$SX_inatt=='', 'SX_inatt'] = 0
+  nv2[nv2$SX_hi=='', 'SX_hi'] = 0
+  nv1[nv1$DOA=='', 'DOA'] = 'blank date'
+  nv2[nv2$DOA=='', 'DOA'] = 'blank date'
+  
+  # and convert any dates not formatted like the majority
+  x = nv1$DOA
+  idx = grepl('-', x)
+  new_date = nv1$DOA
+  new_date[idx] = format(as.Date(x[idx], format='%Y-%m-%d'), format='%m/%d/%Y')
+  idx = grepl('/', new_date) & lapply(new_date, nchar) < 10
+  new_date[idx] = format(as.Date(x[idx], format='%m/%d/%y'), format='%m/%d/%Y')
+  # the remaining ones are already %m/%d/%Y
+  nv1$DOA = new_date
+  x = nv2$DOA
+  idx = grepl('-', x)
+  new_date = nv2$DOA
+  new_date[idx] = format(as.Date(x[idx], format='%Y-%m-%d'), format='%m/%d/%Y')
+  idx = grepl('/', new_date) & lapply(new_date, nchar) < 10
+  new_date[idx] = format(as.Date(x[idx], format='%m/%d/%y'), format='%m/%d/%Y')
+  # the remaining ones are already %m/%d/%Y
+  nv2$DOA = new_date
+  
+  # check if all entries in seed file are still there
+  for (r in 1:nrow(nv1)) {
+    idx = nv2$MRN==nv1[r,'MRN'] & nv2$DOA==nv1[r, 'DOA'] & nv2$SX_inatt==nv1[r, 'SX_inatt'] & nv2$SX_hi==nv1[r, 'SX_hi']
+    if (sum(idx)==0) {
+      print('Cannot find:')
+      print(nv1[r,])
+      print('But found:')
+      idx = nv2$MRN==nv1[r,'MRN'] & nv2$DOA==nv1[r, 'DOA']
+      if (sum(idx) > 0) {
+        print(nv2[idx,])
+      } else {
+        idx = nv2$MRN==nv1[r,'MRN']
+        print(nv2[idx,])
+      }
+    }
+  }
+}
+
+
+sink()
