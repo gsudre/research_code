e5cf2602ee865d8062e9bf141a31a00fdf72efa2
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-07-27 15:12:43 -0400

    some fail checks

--- dti/collect_fatcat_grids.R
+++ dti/collect_fatcat_grids.R
@@ -12,36 +12,41 @@
 
 subj_dir = '~/data/fatcat/parsed/'
 subjs = as.numeric(read.table(sprintf('%s/paul_parsed.txt', subj_dir))[,1])
+expected_rois = 87
+
 data = c()
 for (s in subjs) {
   print(s)
-  subj_data = c()
+  subj_data = c(subj)
   # file numbers with the data matrices
   files = 4:18
   # read in the ROI labels
   titles = read.table(sprintf('%s/%04d.03', subj_dir, s),
                       stringsAsFactors=F)[1,]
-  for (f in files) {
-    fpath = sprintf('%s/%04d.%02d', subj_dir, s, f)
-    # figure out the type of data in the matrix
-    title.line <- readLines(fpath, n=1)
-    dtype = substring(title.line, 3)
-    # read in the actual data and get just the upper triangle
-    a = read.table(fpath)
-    b = a[upper.tri(a, diag=TRUE)]
-    # format a header for the vectorized version of the table
-    w = which(upper.tri(a, diag=TRUE), arr.ind = TRUE)
-    header = sapply(1:length(b),
-                    function(i) sprintf('%s:%s.TO.%s', dtype,
-                                        titles[1, w[i,1]], titles[1,w[i,2]]))
-    names(b) = header
-    # concatenate vectorized matrices for the same mask id in the same row
-    subj_data = c(subj_data, b)    
-  }
-  # stack each mask id vector onto each other as rows 
-  data = rbind(data, subj_data)
+  if length(titles) == expected_rois {
+    for (f in files) {
+        fpath = sprintf('%s/%04d.%02d', subj_dir, s, f)
+        # figure out the type of data in the matrix
+        title.line <- readLines(fpath, n=1)
+        dtype = substring(title.line, 3)
+        # read in the actual data and get just the upper triangle
+        a = read.table(fpath)
+        b = a[upper.tri(a, diag=TRUE)]
+        # format a header for the vectorized version of the table
+        w = which(upper.tri(a, diag=TRUE), arr.ind = TRUE)
+        header = sapply(1:length(b),
+                        function(i) sprintf('%s:%s.TO.%s', dtype,
+                                            titles[1, w[i,1]], titles[1,w[i,2]]))
+        names(b) = header
+        # concatenate vectorized matrices for the same mask id in the same row
+        subj_data = c(subj_data, b)    
+    }
+    # stack each mask id vector onto each other as rows
+    names(subj_data)[1] = 'mask.id'
+    data = rbind(data, subj_data)
+} else {
+    print(sprintf('Ignoring %02d: different number of ROIs (%d)', subj, length(titles)))
 }
 # rename it so that each row is a mask id
-rownames(data) = sapply(subjs, function(x) sprintf('%04d', x))
 save(data, file=sprintf('%s/output.RData', subj_dir))
-write.csv(data, file=sprintf('%s/output.csv', subj_dir))
+write.csv(data, file=sprintf('%s/output.csv', subj_dir), row.names=F)
