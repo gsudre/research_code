0b879697429ecb576db63d3c2f5411aeae854301
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-08-23 15:25:06 -0400

    added script for classifier

---
+++ run_tpot_classifier.py
@@ -0,0 +1,55 @@
+import sys
+import pandas as pd
+import os
+import numpy as np
+from tpot import TPOTClassifier
+from sklearn.dummy import DummyClassifier
+from sklearn import metrics, preprocessing
+
+
+data_fname = sys.argv[1]
+clin_fname = sys.argv[2]
+target = sys.argv[3]
+config_str = sys.argv[4]
+if config_str == 'None':
+    config_str = None
+else:
+    config_str = config_str.replace('_', ' ')
+export_fname = sys.argv[5]
+metric_str = sys.argv[6]
+
+print data_fname
+print clin_fname
+print target
+print config_str
+print export_fname
+print metric_str
+
+home = os.path.expanduser('~')
+cpus = int(os.environ.get('SLURM_CPUS_PER_TASK', '2'))
+data = pd.read_csv(data_fname)
+clin = pd.read_csv(clin_fname)
+
+df = clin.merge(data, on='mask.id')
+voxels = [cname for cname in data.columns if cname.find('v')==0]
+X = df[voxels]
+y = df[target]
+# TPOT doesn't like categorical variables
+lbl = preprocessing.LabelEncoder()
+y2 = lbl.fit_transform(y)
+
+tpot = TPOTClassifier(verbosity=2,
+                     config_dict=config_str, n_jobs=cpus, 
+                     periodic_checkpoint_folder=home+'/data/tpot/checkpoints',
+                     memory='auto', random_state=42,
+                     scoring=metric_str)
+
+dummy = DummyClassifier()
+dummy.fit(X, y2)
+ypred = dummy.predict(X)
+scr = metrics.get_scorer(metric_str)
+
+print 'CPUs:', cpus
+print 'dummy:', scr(dummy, y2.reshape(-1, 1), ypred)
+tpot.fit(X, y2)
+tpot.export(export_fname)
