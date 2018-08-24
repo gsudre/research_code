086682ee5e581c5a348c761e59167d263b7fd3cf
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-08-24 16:22:37 -0400

    another try with forkserver

--- run_tpot_regressor.py
+++ run_tpot_regressor.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import sys
 import pandas as pd
 import os
@@ -6,7 +7,6 @@ from scipy import stats
 from tpot import TPOTRegressor
 from sklearn.dummy import DummyRegressor
 from sklearn import metrics
-import multiprocessing
 
 
 data_fname = sys.argv[1]
@@ -31,29 +31,30 @@ print(export_fname)
 
 home = os.path.expanduser('~')
 
+data = pd.read_csv(data_fname)
+clin = pd.read_csv(clin_fname)
+
+df = clin.merge(data, on='mask.id')
+voxels = [cname for cname in data.columns if cname.find('v')==0]
+junk = stats.mstats.winsorize(df[target],inplace=True,limits=(.01, .01))
+X = df[voxels]
+y = df[target]
+
+tpot = TPOTRegressor(verbosity=2,
+                    config_dict=config_str, n_jobs=cpus, 
+                    periodic_checkpoint_folder=home+'/data/tpot/checkpoints',
+                    memory='auto', random_state=42)
+
+dummy = DummyRegressor()
+dummy.fit(X, y)
+ypred = dummy.predict(X)
+
+print('CPUs:', cpus)
+print('dummy:', -metrics.mean_squared_error(y, ypred))
+
 if __name__ == '__main__':
     multiprocessing.set_start_method('forkserver')
-    
-    data = pd.read_csv(data_fname)
-    clin = pd.read_csv(clin_fname)
-
-    df = clin.merge(data, on='mask.id')
-    voxels = [cname for cname in data.columns if cname.find('v')==0]
-    junk = stats.mstats.winsorize(df[target],inplace=True,limits=(.01, .01))
-    X = df[voxels]
-    y = df[target]
-
-    tpot = TPOTRegressor(verbosity=2,
-                        config_dict=config_str, n_jobs=cpus, 
-                        periodic_checkpoint_folder=home+'/data/tpot/checkpoints',
-                        memory='auto', random_state=42)
-
-    dummy = DummyRegressor()
-    dummy.fit(X, y)
-    ypred = dummy.predict(X)
-
-    print('CPUs:', cpus)
-    print('dummy:', -metrics.mean_squared_error(y, ypred))
 
     tpot.fit(X, y)
+
     tpot.export(export_fname)
