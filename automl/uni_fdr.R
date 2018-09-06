92c8963bf9de04f0848fa417d78d4fd8a9d85f89
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-09-06 15:54:49 -0400

    adding fdr to variables

---
+++ automl/uni_fdr.R
@@ -0,0 +1,78 @@
+# runs classification tasks using AutoML from H2O
+
+args <- commandArgs(trailingOnly = TRUE)
+data_fname = args[1]
+clin_fname = args[2]
+target = args[3]
+export_fname = args[4]
+
+
+winsorize = function(x, cut = 0.01){
+  cut_point_top <- quantile(x, 1 - cut, na.rm = T)
+  cut_point_bottom <- quantile(x, cut, na.rm = T)
+  i = which(x >= cut_point_top) 
+  x[i] = cut_point_top
+  j = which(x <= cut_point_bottom) 
+  x[j] = cut_point_bottom
+  return(x)
+}
+
+# starting h2o
+library(h2o)
+h2o.init(ip='localhost', nthreads=future::availableCores(), max_mem_size='30G')
+
+# merging phenotype and clinical data
+clin = h2o.importFile(clin_fname)
+data = h2o.importFile(data_fname)
+df = h2o.merge(clin, data, by='mask.id')
+
+
+x = colnames(df)[grepl(pattern = '^v', colnames(df))]
+a = cbind(as.matrix(df[, x]), as.vector(df[, target]))
+colnames(a)[ncol(a)] = target
+
+# winsorize and get univariates if it's a continuous variable
+if (! grepl(pattern = 'group', target)) {
+  b = sapply(as.data.frame(a[,x]),
+             function(myx) {
+               res = cor.test(myx, a[, target], method='spearman');
+               return(res$p.value)
+             })
+  # winsorizing after correlations to avoid ties
+  a[, target] = winsorize(a[, target])
+} else {
+  a[, target] = as.factor(a[, target])
+  b = sapply(as.data.frame(a[,x]),
+             function(myx) {
+               res = kruskal.test(myx, a[, target]);
+               return(res$p.value)
+             })
+}
+b = p.adjust(b, method='fdr')
+keep_me = b <= .05
+a = a[, keep_me]
+
+print(dim(a))
+
+a = cbind(a, as.vector(df[, target]))
+colnames(a)[ncol(a)] = target
+
+# transform it back to h2o data frame
+df2 = as.h2o(a)
+# groups need to be factors, after the h2o dataframe is created!
+if (grepl(pattern = 'group', target)) {
+  df2[, target] = as.factor(df2[, target])
+}
+
+x = colnames(df2)[grepl(pattern = '^v', colnames(df2))]
+aml <- h2o.automl(x = x, y = target, training_frame = df2,
+                  seed=42,
+                  max_runtime_secs = NULL,
+                  max_models = NULL)
+
+print(data_fname)
+print(clin_fname)
+print(target)
+print(dim(df2))
+print(aml@leaderboard)
+h2o.saveModel(aml@leader, path = export_fname)
