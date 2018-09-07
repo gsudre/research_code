c9cba8e8f509f0e183e6ece0f01dc8d94ef715c9
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-09-07 17:33:15 -0400

    changed to use helix

--- lab_mgmt/setup_diffprep_biowulf.sh
+++ lab_mgmt/setup_diffprep_biowulf.sh
@@ -16,19 +16,19 @@ while read m; do
 
     # piping inside the loop was breaking it. Will need to do it later. -n flag didn't work because I actually need the stdin pipe.
     echo "echo \"Copying over eDTI files for ${m}\"" >> $tmp_script
-    echo "ssh -q biowulf.nih.gov \"mkdir ${bw_dir}/${m}\"" >> $tmp_script
+    echo "ssh -q helix.nih.gov \"mkdir ${bw_dir}/${m}\"" >> $tmp_script
     echo "cd ${data_dir}/${m}" >> $tmp_script
     if [ -d ${data_dir}/${m}/edti_proc ]; then
-        echo "gtar czf - edti edti_proc/edti.* | ssh -q biowulf.nih.gov \"cd ${bw_dir}/${m}; tar xzf -\"" >> $tmp_script
+        echo "gtar czf - edti edti_proc/edti.* | ssh -q helix.nih.gov \"cd ${bw_dir}/${m}; tar xzf -\"" >> $tmp_script
     else
         echo "Could not find edti_proc";
-        echo "gtar czf - edti | ssh -q biowulf.nih.gov \"cd ${bw_dir}/${m}; tar xzf -\"" >> $tmp_script
+        echo "gtar czf - edti | ssh -q helix.nih.gov \"cd ${bw_dir}/${m}; tar xzf -\"" >> $tmp_script
     fi
 
     # setting up XML file
     cp ~/tortoise_in_biowulf/0000_template.xml ~/tortoise_in_biowulf/${m}.xml
     perl -p -i -e "s/0000/${m}/g" ~/tortoise_in_biowulf/${m}.xml
-    scp -q ~/tortoise_in_biowulf/${m}.xml bw:~/tortoiseXMLfiles/
+    scp -q ~/tortoise_in_biowulf/${m}.xml helix.nih.gov:~/tortoiseXMLfiles/
     rm ~/tortoise_in_biowulf/${m}.xml
 
     # adding mask id to the current batch file
@@ -39,7 +39,7 @@ while read m; do
 
     echo "wait" >> ${batchFile}
     mv ${batchFile} ~/tortoise_in_biowulf/tortoise${suffix}.bat
-    scp -q ~/tortoise_in_biowulf/tortoise${suffix}.bat bw:~/scripts/
+    scp -q ~/tortoise_in_biowulf/tortoise${suffix}.bat helix.nih.gov:~/scripts/
 done < ${maskids}
 
 echo "cd ${start_dir}" >> $tmp_script
