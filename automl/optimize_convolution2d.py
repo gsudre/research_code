d0a9727e4b960cd81a117a5b700647d61bd21a3a
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-10-24 16:46:47 -0400

    new function to train autoencoders

---
+++ automl/optimize_convolution2d.py
@@ -0,0 +1,78 @@
+import keras
+from matplotlib import pyplot as plt
+import numpy as np
+import gzip
+#%matplotlib inline
+from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D
+from keras.models import Model
+from keras.optimizers import RMSprop
+
+
+def extract_data(filename, num_images):
+    with gzip.open(filename) as bytestream:
+        bytestream.read(16)
+        buf = bytestream.read(28 * 28 * num_images)
+        data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+        data = data.reshape(num_images, 28,28)
+        return data
+
+
+train_data = extract_data('/Users/sudregp/Downloads/train-images-idx3-ubyte.gz', 60000)
+test_data = extract_data('/Users/sudregp/Downloads/t10k-images-idx3-ubyte.gz', 10000)
+
+train_data = train_data.reshape(-1, 28,28, 1)
+test_data = test_data.reshape(-1, 28,28, 1)
+train_data.shape, test_data.shape
+
+train_data = train_data / np.max(train_data)
+test_data = test_data / np.max(test_data)
+
+from sklearn.model_selection import train_test_split
+train_X,valid_X,train_ground,valid_ground = train_test_split(train_data,
+                                                             train_data, 
+                                                             test_size=0.2, 
+                                                             random_state=13)
+
+
+batch_size = 1000
+epochs = 20
+inChannel = 1
+x, y = 28, 28
+input_img = Input(shape = (x, y, inChannel))
+
+def autoencoder(input_img):
+    #encoder
+    #input = 28 x 28 x 1 (wide and thin)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+
+    #decoder
+    conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 128
+    up1 = UpSampling2D((2,2))(conv4) # 14 x 14 x 128
+    conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
+    up2 = UpSampling2D((2,2))(conv5) # 28 x 28 x 64
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+    return decoded
+
+
+autoencoder = Model(input_img, autoencoder(input_img))
+autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+
+autoencoder.summary()
+
+autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_ground))
+
+loss = autoencoder_train.history['loss']
+val_loss = autoencoder_train.history['val_loss']
+epochs = range(epochs)
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+pred = autoencoder.predict(test_data)
---
+++ automl/optimize_convolution3d.py
@@ -0,0 +1,59 @@
+import keras
+from matplotlib import pyplot as plt
+import numpy as np
+import gzip
+#%matplotlib inline
+from keras.layers import Input,Conv3D,MaxPooling3D,UpSampling3D
+from keras.models import Model
+from keras.optimizers import RMSprop
+import numpy as np
+import os
+
+
+home = os.path.expanduser('~')
+data = np.load(home + '/data/tmp/ad_223_3d.npz')['data']
+
+data2 = data[:, :, :, :, np.newaxis]
+data2 = data2 / np.max(data2)
+
+batch_size = 5
+epochs = 20
+inChannel = 1
+x, y, z = data.shape[1:]
+input_img = Input(shape = (x, y, z, inChannel))
+
+def autoencoder(input_img):
+    #encoder
+    conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(input_img)
+    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conv1) 
+    conv2 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(pool1)
+    pool2 = MaxPooling3D(pool_size=(2, 2, 2))(conv2) 
+    conv3 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(pool2)
+
+    #decoder
+    conv4 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conv3)
+    up1 = UpSampling3D((2,2,2))(conv4)
+    conv5 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(up1) 
+    up2 = UpSampling3D((2,2,2))(conv5)
+    decoded = Conv3D(1, (3, 3, 3), activation='sigmoid', padding='same')(up2)
+    return decoded
+
+
+autoencoder = Model(input_img, autoencoder(input_img))
+autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+
+autoencoder.summary()
+
+autoencoder_train = autoencoder.fit(data2, data2, batch_size=batch_size,
+                                    epochs=epochs, verbose=1,
+                                    validation_split=.1)
+
+loss = autoencoder_train.history['loss']
+val_loss = autoencoder_train.history['val_loss']
+epochs = range(epochs)
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
