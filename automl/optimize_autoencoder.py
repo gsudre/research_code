9644e3e1a8228cc1ed191168678a14afe00b6396
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-10-24 17:29:59 -0400

    removing cross validation

--- automl/optimize_autoencoder.py
+++ automl/optimize_autoencoder.py
@@ -8,6 +8,9 @@ from keras.layers import Dropout, Input
 from keras.wrappers.scikit_learn import KerasRegressor
 from keras.constraints import maxnorm
 from keras import regularizers, Model
+import os
+
+
 # Function to create model, required for KerasClassifier
 def create_model(neurons=1):
 	# # create model
@@ -44,16 +47,41 @@ scaler.fit(dataset)
 X_scaled = scaler.transform(dataset)
 
 # create model
-model = KerasRegressor(build_fn=create_model, epochs=400, batch_size=16, verbose=0, shuffle=True, validation_split=.1)
+# model = KerasRegressor(build_fn=create_model, epochs=400, batch_size=16, verbose=0, shuffle=True, validation_split=.1)
 # define the grid search parameters
-neurons = [1, 5, 10, 15, 20, 25, 30]
-param_grid = dict(neurons=neurons)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=2)
-grid_result = grid.fit(X_scaled, X_scaled)
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+# neurons = [1, 5, 10, 15, 20, 25, 30]
+# param_grid = dict(neurons=neurons)
+# grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=2)
+# grid_result = grid.fit(X_scaled, X_scaled)
+
+
+# # summarize results
+# print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print("%f (%f) with: %r" % (mean, stdev, param))
+
+autoencoder = create_model(neurons=30)
+autoencoder.summary()
+
+batch_size = 5
+epochs = 20
+
+autoencoder_train = autoencoder.fit(X_scaled, Xscaled, batch_size=batch_size,
+                                    epochs=epochs, verbose=1,
+                                    validation_split=.1)
+
+# save the model so we don't lose all this work in case of accidents
+autoencoder.save(home + "/tmp/model_autoencoder.h5py")
+
+loss = autoencoder_train.history['loss']
+val_loss = autoencoder_train.history['val_loss']
+epochs = range(epochs)
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
