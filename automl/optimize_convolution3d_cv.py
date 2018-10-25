3b50883e4fd39be1819b0a2807553541510137ee
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-10-25 15:30:20 -0400

    cv on init_mode

--- automl/optimize_convolution3d_cv.py
+++ automl/optimize_convolution3d_cv.py
@@ -23,27 +23,29 @@ inChannel = 1
 x, y, z = data.shape[1:]
 input_img = Input(shape = (x, y, z, inChannel))
 
-def create_model(activation):
+def create_model(init_mode):
+    activation = 'relu'
+    # init_mode = 'glorot_uniform'
     #encoder
-    x = Conv3D(32, (3, 3, 3), activation=activation, padding='same')(input_img)
+    x = Conv3D(32, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(input_img)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x) 
-    x = Conv3D(16, (3, 3, 3), activation=activation, padding='same')(x)
+    x = Conv3D(16, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x) 
-    x = Conv3D(8, (3, 3, 3), activation=activation, padding='same')(x)
+    x = Conv3D(8, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x) 
-    x = Conv3D(4, (3, 3, 3), activation=activation, padding='same')(x)
+    x = Conv3D(4, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x)
     encoded = MaxPooling3D(pool_size=(7, 7, 3))(x)
 
     #decoder
-    x = Conv3D(4, (3, 3, 3), activation=activation, padding='same')(encoded)
+    x = Conv3D(4, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(encoded)
     x = UpSampling3D((7,7,3))(x)
-    x = Conv3D(8, (3, 3, 3), activation=activation, padding='same')(x)
+    x = Conv3D(8, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x)
     x = UpSampling3D((2,2,2))(x)
-    x = Conv3D(16, (3, 3, 3), activation=activation, padding='same')(x) 
+    x = Conv3D(16, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x) 
     x = UpSampling3D((2,2,2))(x)
-    x = Conv3D(32, (3, 3, 3), activation=activation, padding='same')(x)
+    x = Conv3D(32, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x)
     x = UpSampling3D((2,2,2))(x)
-    decoded = Conv3D(1, (3, 3, 3), activation=activation, padding='same')(x)
+    decoded = Conv3D(1, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x)
 
     autoencoder = Model(input_img, decoded)
     autoencoder.compile(loss='mse', optimizer = 'adadelta')
@@ -56,9 +58,12 @@ np.random.seed(seed)
 # create model
 model = KerasRegressor(build_fn=create_model, epochs=epochs, batch_size=batch_size, verbose=1)
 # define the grid search parameters
-params = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
-param_grid = dict(activation=params)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
+# params = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid',
+# 'hard_sigmoid', 'linear']
+# param_grid = dict(activation=params)
+params = ['uniform', 'lecun_uniform', 'normal', 'zero', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
+param_grid = dict(init_mode=params)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=5)
 grid_result = grid.fit(data2, data2)
 
 # summarize results
