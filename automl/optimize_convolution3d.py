641821c3d42b68c46b65476524dce4d5b7c69fc8
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-10-25 13:54:40 -0400

    deepening 3d convolution

--- automl/optimize_convolution3d.py
+++ automl/optimize_convolution3d.py
@@ -22,25 +22,30 @@ inChannel = 1
 x, y, z = data.shape[1:]
 input_img = Input(shape = (x, y, z, inChannel))
 
-def autoencoder(input_img):
-    #encoder
-    conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(input_img)
-    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conv1) 
-    conv2 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(pool1)
-    pool2 = MaxPooling3D(pool_size=(2, 2, 2))(conv2) 
-    conv3 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(pool2)
+#encoder
+x = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling3D(pool_size=(2, 2, 2))(x) 
+x = Conv3D(16, (3, 3, 3), activation='relu', padding='same')(x)
+x = MaxPooling3D(pool_size=(2, 2, 2))(x) 
+x = Conv3D(8, (3, 3, 3), activation='relu', padding='same')(x)
+x = MaxPooling3D(pool_size=(2, 2, 2))(x) 
+x = Conv3D(4, (3, 3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling3D(pool_size=(7, 7, 3))(x)
 
-    #decoder
-    conv4 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conv3)
-    up1 = UpSampling3D((2,2,2))(conv4)
-    conv5 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(up1) 
-    up2 = UpSampling3D((2,2,2))(conv5)
-    decoded = Conv3D(1, (3, 3, 3), activation='sigmoid', padding='same')(up2)
-    return decoded
+#decoder
+x = Conv3D(4, (3, 3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling3D((7,7,3))(x)
+x = Conv3D(8, (3, 3, 3), activation='relu', padding='same')(x)
+x = UpSampling3D((2,2,2))(x)
+x = Conv3D(16, (3, 3, 3), activation='relu', padding='same')(x) 
+x = UpSampling3D((2,2,2))(x)
+x = Conv3D(32, (3, 3, 3), activation='sigmoid', padding='same')(x)
+x = UpSampling3D((2,2,2))(x)
+decoded = Conv3D(1, (3, 3, 3), activation='sigmoid', padding='same')(x)
 
 
-autoencoder = Model(input_img, autoencoder(input_img))
-autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(loss='binary_crossentropy', optimizer = 'adadelta')
 
 autoencoder.summary()
 
@@ -51,12 +56,12 @@ autoencoder_train = autoencoder.fit(data2, data2, batch_size=batch_size,
 # save the model so we don't lose all this work in case of accidents
 autoencoder.save(home + "/tmp/model_3dconv.h5py")
 
-loss = autoencoder_train.history['loss']
-val_loss = autoencoder_train.history['val_loss']
-epochs = range(epochs)
-plt.figure()
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
+# loss = autoencoder_train.history['loss']
+# val_loss = autoencoder_train.history['val_loss']
+# epochs = range(epochs)
+# plt.figure()
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.legend()
+# plt.show()
