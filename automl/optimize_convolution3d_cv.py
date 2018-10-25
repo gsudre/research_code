7184594f79e53ab56e521b716798d5305235a860
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-10-25 15:46:44 -0400

    cv on optimizer

--- automl/optimize_convolution3d_cv.py
+++ automl/optimize_convolution3d_cv.py
@@ -23,7 +23,7 @@ inChannel = 1
 x, y, z = data.shape[1:]
 input_img = Input(shape = (x, y, z, inChannel))
 
-def create_model(init_mode='glorot_uniform', activation='relu'):
+def create_model(init_mode='glorot_uniform', activation='relu', optimizer='adadelta'):
     #encoder
     x = Conv3D(32, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(input_img)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x) 
@@ -46,7 +46,7 @@ def create_model(init_mode='glorot_uniform', activation='relu'):
     decoded = Conv3D(1, (3, 3, 3), activation=activation, kernel_initializer=init_mode, padding='same')(x)
 
     autoencoder = Model(input_img, decoded)
-    autoencoder.compile(loss='mse', optimizer = 'adadelta')
+    autoencoder.compile(loss='mse', optimizer = optimizer)
     return autoencoder
 
 
@@ -56,9 +56,11 @@ np.random.seed(seed)
 # create model
 model = KerasRegressor(build_fn=create_model, epochs=epochs, batch_size=batch_size, verbose=1, validation_split=.1)
 # define the grid search parameters
-params = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid',
-'hard_sigmoid', 'linear']
-param_grid = dict(activation=params)
+# params = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid',
+# 'hard_sigmoid', 'linear']
+# param_grid = dict(activation=params)
+params = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+param_grid = dict(optimizer=params)
 # params = ['uniform', 'lecun_uniform', 'normal', 'zero', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
 # param_grid = dict(init_mode=params)
 grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=5)
