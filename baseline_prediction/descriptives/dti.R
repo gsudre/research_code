abd2f993b181046fcc763d99154f126e4b566f8e
Gustavo Sudre <gustavo.sudre@nih.gov>
2018-11-30 12:43:55 -0500

    working version locally of dti descriptives

--- baseline_prediction/descriptives/dti.R
+++ baseline_prediction/descriptives/dti.R
@@ -1,16 +1,18 @@
 # generates text files with results from univariate tests on randomized labels,
 # to be later used to assess biggest cluster
 
-# args <- commandArgs(trailingOnly = TRUE)
-# data_fname = args[1]
-# clin_fname = args[2]
-# target = args[3]
-# myseed = as.numeric(args[4])
+args <- commandArgs(trailingOnly = TRUE)
+data_fname = args[1]
+clin_fname = args[2]
+target = args[3]
+myseed = as.numeric(args[4])
+preproc = args[5]
 
-data_fname = '~/data/baseline_prediction/dti_ad_voxelwise_n223_09212018.RData.gz'
-clin_fname = '~/data/baseline_prediction/long_clin_0918.csv'
-target = 'nvVSadhd'
-myseed = 42
+# data_fname = '~/data/baseline_prediction/dti_ad_voxelwise_n272_09212018.RData.gz'
+# clin_fname = '~/data/baseline_prediction/long_clin_11302018.csv'
+# target = 'SX_inatt_baseline'
+# myseed = -1234
+# preproc = 'subjScale'
 
 winsorize = function(x, cut = 0.01){
   cut_point_top <- quantile(x, 1 - cut, na.rm = T)
@@ -22,7 +24,13 @@ winsorize = function(x, cut = 0.01){
   return(x)
 }
 
-base_name = '~/data/'
+if (Sys.info()['sysname'] == 'Darwin') {
+  max_mem = '16G'
+  base_name = '~/data/'
+} else {
+  max_mem = paste(Sys.getenv('SLURM_MEM_PER_NODE'),'m',sep='')
+  base_name = '/data/NCR_SBRB/'
+}
 print('Loading files')
 # merging phenotype and clinical data
 clin = read.csv(clin_fname)
@@ -43,7 +51,7 @@ print('Merging files')
 df = merge(clin, data, by='MRN')
 print('Looking for data columns')
 x = colnames(df)[grepl(pattern = '^v', colnames(df))]
-dti = read.csv('~/data/baseline_prediction/dti_long_09272018.csv')
+dti = read.csv(sprintf('%s/baseline_prediction/dti_long_09272018.csv', base_name))
 df = merge(df, dti, by='mask.id')
 df$mvmt = rowMeans(scale(df$norm.trans), scale(df$norm.rot))
 
@@ -53,6 +61,7 @@ df$mvmt = rowMeans(scale(df$norm.trans), scale(df$norm.rot))
 # for pairwise, we can do the usual nvVSper, nvVSrem, perVSrem, then
 # groupOLS_SX_slope_ plus nvVSimp, nvVSnonimp, impVSnonimp, plus all of them
 # using nonew_
+input_target = target
 if (grepl('nonew', target)) {
   df = df[df$diag_group != 'new_onset', ]
   df$diag_group = factor(df$diag_group)
@@ -74,7 +83,6 @@ if (grepl('ADHDNOS', target)) {
   }
 }
 
-input_target = target
 # pairwise comparisons
 if (grepl('VS', target)) {
     df[, 'groupSlope'] = 'nonimprovers'
@@ -127,154 +135,77 @@ if (grepl('VS', target)) {
     print(table(df[, target]))
 }
 
-# CHANGE THIS TO USE FAMID! NEED TO IMPORT FAMID TO CLINICAL FILE!
-# SHOULD ALSO CHANGE THIS TO OUTPUT 3DCLUST RESULTS REGARDLESS OF SEED, SO WE
-# CAN RUN THE SAME FILE FOR BOTH RANDOMIZED AND NON-RANDOMIZED DATA.
+# shuffling labels around if needed
+if (myseed > 0) {
+    idx = 1:nrow(df)
+    suffix = ''
+} else {
+    myseed = -myseed
+    set.seed(myseed)
+    idx = sample(1:nrow(df), nrow(df), replace=F)
+    suffix = 'rnd_'
+}
+df[, target] = df[idx, target]
+
+if (grepl(pattern='subjScale', preproc)) {
+    print('Normalizing within subjects')
+    df[, x] = t(scale(t(df[, x])))
+}
 
 # first, generate main result
 ps = vector()
 ts = vector()
 bs = vector()
-library(lme4)
+set.seed(myseed)
+library(nlme)
 for (v in x) {
-    mydata = df[, c(target, 'Sex', 'mvmt', 'age_at_scan', 'numVolumes')]
-    mydata$y = df[,v]
-    fm = as.formula(sprintf("y ~ %s + Sex + mvmt + I(mvmt^2) + age_at_scan + I(age_at_scan^2) + (1|numVolumes)", target))
-    fit = lmer(fm, data=mydata, na.action=na.omit)
-    ps = c(ps, summary(fit)$tTable[2,5])
-    ts = c(ts, summary(fit)$tTable[2,4])
-    bs = c(bs, summary(fit)$tTable[2,1])
+    # print(v)
+    mydata = df[, c(target, 'Sex', 'mvmt', 'age_at_scan', 'nuclearFamID')]
+    if (grepl(pattern='log', preproc)) {
+        mydata$y = log(df[,v])
+    } else {
+        mydata$y = df[,v]
+    }
+    fm = as.formula(sprintf("y ~ %s + Sex + mvmt + I(mvmt^2) + age_at_scan + I(age_at_scan^2)", target))
+    fit = try(lme(fm, random=~1|nuclearFamID, data=mydata, na.action=na.omit))
+    if (length(fit) > 1) {
+        ps = c(ps, summary(fit)$tTable[2,5])
+        ts = c(ts, summary(fit)$tTable[2,4])
+        bs = c(bs, summary(fit)$tTable[2,1])
+    } else {
+        ps = c(ps, 1)
+        ts = c(ts, 0)
+        bs = c(bs, 0)
+    }
 }
-summary(ps)
 
-# then shuffle targets but keep data constant so we can keep the data and
-# covariates relationship constant
+# write 1-p images and do clustering
+if (grepl(pattern='223', data_fname)) {
+    ijk_fname = sprintf('%s/baseline_prediction/dti_223_ijk.txt', base_name)
+    mask_fname = sprintf('%s/baseline_prediction/mean_223_fa_skeleton_mask.nii.gz',
+                        base_name)
+} else {
+    ijk_fname = sprintf('%s/baseline_prediction/dti_272_ijk.txt', base_name)
+    mask_fname = sprintf('%s/baseline_prediction/mean_272_fa_skeleton_mask.nii.gz',
+                        base_name)
+}
+out = read.table(ijk_fname)
+out[, 4] = 0
+keep_me = ps < .05
+out[keep_me, 4] = 1
 
-
-
-# # shuffling labels around
-# if (myseed < 0) {
-#     # don't shuffle if seed is negative!
-#     myseed = -myseed
-#     idx = 1:nrow(df)
-# } else {
-#     set.seed(myseed)
-#     idx = sample(1:nrow(df), nrow(df), replace=F)
-# }
-# df[, target] = df[idx, target]
-
-# data.train = df
-
-# print('Running univariate analysis')
-# # winsorize and get univariates if it's a continuous variable
-# if (! grepl(pattern = 'group', target)) {
-#     b = sapply(data.train[, x],
-#                 function(myx) {
-#                 res = cor.test(myx, data.train[, target], method='spearman');
-#                 return(res$p.value)
-#                 })
-#     # winsorizing after correlations to avoid ties
-#     data.train[, target] = winsorize(data.train[, target])
-# } else {
-#     data.train[, target] = as.factor(data.train[, target])
-#     b = sapply(data.train[,x],
-#                 function(myx) {
-#                 res = kruskal.test(myx, data.train[, target]);
-#                 return(res$p.value)
-#                 })
-# }
-# keep_me = b <= .05
-# print(sprintf('Variables before univariate filter: %d', length(x)))
-# print(sprintf('Variables after univariate filter: %d', sum(keep_me)))
-
-# # further filter variables to keep only the ones clustered together
-# if (grepl(pattern='dti', data_fname)) {
-#     if (grepl(pattern='223', data_fname)) {
-#         ijk_fname = sprintf('%s/baseline_prediction/dti_223_ijk.txt', base_name)
-#         mask_fname = sprintf('%s/baseline_prediction/mean_223_fa_skeleton_mask.nii.gz',
-#                             base_name)
-#     } else {
-#         ijk_fname = sprintf('%s/baseline_prediction/dti_272_ijk.txt', base_name)
-#         mask_fname = sprintf('%s/baseline_prediction/mean_272_fa_skeleton_mask.nii.gz',
-#                             base_name)
-#     }
-#     out_dir = sprintf('%s/tmp/', base_name)
-#     out = read.table(ijk_fname)
-#     out[, 4] = 0
-#     out[keep_me, 4] = 1
-#     out_fname = sprintf('%s/%s_%d', out_dir, input_target, myseed)
-#     # writing good voxels to be clustered
-#     write.table(out, file=sprintf('%s.txt', out_fname), row.names=F, col.names=F)
-#     cmd_line = sprintf('cat %s.txt | 3dUndump -master %s -ijk -datum float -prefix %s -overwrite -;',
-#                         out_fname, mask_fname, out_fname)
-#     system(cmd_line)
-#     # spit out all clusters
-#     cmd_line = sprintf('3dclust -NN1 1 -orient LPI %s+orig 2>/dev/null > %s_rnd_clusters.txt',
-#                         out_fname, out_fname, out_fname)
-#     system(cmd_line)
-# } else if (grepl(pattern='struct', data_fname)) {
-#         # structural is a bit more challenging because left and right are separate
-#         nvox = length(x_orig)
-#         out_dir = sprintf('%s/tmp/', base_name)
-#         out = rep(0, nvox)
-#         names(out) = x_orig
-#         out[x[keep_me]] = 1
-#         out_fname = sprintf('%s/lh_%s_ds%d_%d', out_dir, input_target, f, myseed)
-
-#         # writing good voxels to be clustered. left hemisphere first
-#         write.table(out[1:(nvox/2)], file=sprintf('%s.txt', out_fname), row.names=F, col.names=F)
-        
-#         # spit out all clusters >= min_cluster
-#         cmd_line = sprintf('SurfClust -i %s/freesurfer5.3_subjects/fsaverage/SUMA/lh.pial.asc -input %s.txt 0 -rmm -1.000000 -thresh_col 0 -athresh .95 -sort_area -no_cent -prefix %s_lh -out_roidset -out_fulllist -amm2 %s',
-#             base_name, out_fname, out_fname, min_clusters[[1]][f])
-#         system(cmd_line)
-#         # read mask back in and filter x properly
-#         clus = read.table(sprintf('%s_lh_ClstMsk_e1_a%s.0.niml.dset', out_fname, min_clusters[[1]][f]),
-#                         skip=12, nrows=(nvox/2))[[1]]
-#         lh_cluster_idx = clus > 0
-
-#         # let's average within each cluster, and only keep those variables as good
-#         my_clusters = unique(clus[lh_cluster_idx])
-#         new_x = c()
-#         x_lh = x_orig[grepl(pattern = 'lh', x_orig)]
-#         for (cl in 1:length(my_clusters)) {
-#             cluster_vars = clus == my_clusters[cl]
-#             cl_avg = rowMeans(data.train[, x_lh[cluster_vars]])
-#             data.train = cbind(data.train, cl_avg)
-#             colnames(data.train)[ncol(data.train)] = sprintf('v_ds%dLHclAvg%03d', f, cl)
-#             cl_avg = rowMeans(data.test[, x_lh[cluster_vars]])
-#             data.test = cbind(data.test, cl_avg)
-#             colnames(data.test)[ncol(data.test)] = sprintf('v_ds%dLHclAvg%03d', f, cl)
-#             new_x = c(new_x, sprintf('v_ds%dLHclAvg%03d', f, cl))
-#         }
-
-#         # now, repeat the exact same thing for right hemisphere
-#         out_fname = sprintf('%s/rh_%s_ds%d_%d', out_dir, input_target, f, myseed)
-
-#         write.table(out[(nvox/2+1):length(out)], file=sprintf('%s.txt', out_fname), row.names=F, col.names=F)
-#         cmd_line = sprintf('SurfClust -i %s/freesurfer5.3_subjects/fsaverage/SUMA/rh.pial.asc -input %s.txt 0 -rmm -1.000000 -thresh_col 0 -athresh .95 -sort_area -no_cent -prefix %s_rh -out_roidset -out_fulllist -amm2 %s',
-#             base_name, out_fname, out_fname, min_clusters[[1]][f])
-#         system(cmd_line)
-#         clus = read.table(sprintf('%s_rh_ClstMsk_e1_a%s.0.niml.dset', out_fname, min_clusters[[1]][f]),
-#                         skip=12, nrows=(nvox/2))[[1]]
-#         rh_cluster_idx = clus > 0
-
-#         my_clusters = unique(clus[rh_cluster_idx])
-#         x_rh = x_orig[grepl(pattern = 'rh', x_orig)]
-#         for (cl in 1:length(my_clusters)) {
-#             cluster_vars = clus == my_clusters[cl]
-#             cl_avg = rowMeans(data.train[, x_rh[cluster_vars]])
-#             data.train = cbind(data.train, cl_avg)
-#             colnames(data.train)[ncol(data.train)] = sprintf('v_ds%dRHclAvg%03d', f, cl)
-#             cl_avg = rowMeans(data.test[, x_rh[cluster_vars]])
-#             data.test = cbind(data.test, cl_avg)
-#             colnames(data.test)[ncol(data.test)] = sprintf('v_ds%dRHclAvg%03d', f, cl)
-#             new_x = c(new_x, sprintf('v_ds%dRHclAvg%03d', f, cl))
-#         }
-
-#         cluster_idx = c(lh_cluster_idx, rh_cluster_idx)
-#         print(sprintf('Variables after spatial filter: %d', sum(cluster_idx)))
-
-#         # disregard all other variables and keep only the cluster averages
-#         x = new_x
-#     }
+junk = strsplit(data_fname, '/')[[1]]
+pheno = strsplit(junk[length(junk)], '\\.')[[1]][1]
+out_dir = sprintf('%s/tmp/%s/', base_name, pheno)
+system(sprintf('mkdir %s', out_dir))
+out_fname = sprintf('%s/%s_%s_%s%d', out_dir, input_target, preproc, suffix, myseed)
+save(ps, ts, bs, file=sprintf('%s.RData', out_fname))
+# writing good voxels to be clustered
+write.table(out, file=sprintf('%s.txt', out_fname), row.names=F, col.names=F)
+cmd_line = sprintf('cat %s.txt | 3dUndump -master %s -ijk -datum float -prefix %s -overwrite -;',
+                    out_fname, mask_fname, out_fname)
+system(cmd_line)
+# spit out all clusters
+cmd_line = sprintf('3dclust -NN1 1 -orient LPI %s+orig 2>/dev/null > %s_clusters.txt',
+                    out_fname, out_fname, out_fname)
+system(cmd_line)
