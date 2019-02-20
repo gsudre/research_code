405829d2238a1196a084727ccfe139f8e8b6223a
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-02-20 13:14:12 -0500

    first working version of edyd script for NCR

--- dti/fdt_ncr_eddy.sh
+++ dti/fdt_ncr_eddy.sh
@@ -29,15 +29,27 @@ idx=''; for i in `seq 1 $nvol`; do
     a=$a' '1;
 done;
 echo $a > index.txt
+
+# according to the edyd webpage it doesn't matter what we have for acquisition
+# parameters.
+# https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/eddy/Faq#How_do_I_know_what_to_put_into_my_--acqp_file
+# So, let's stick with the same stuff we used for PNC. But actually, the JSON
+# coming from our data is quite similar to the PNC one, so I'm not too worried
+# about this.
 echo "0 -1 0 0.102" > acqparams.txt
 
 # eddy_openmp --imain=dwi --mask=b0_brain_mask --index=index.txt \
 #     --acqp=acqparams.txt --bvecs=dwi_cvec.dat --bvals=dwi_bval.dat \
 #     --fwhm=0 --flm=quadratic --out=eddy_unwarped_images --cnr_maps --repol --mporder=6
 
-cp ../my_slspec.txt ./
-eddy_cuda --imain=dwi --acqp=acqparams.txt --index=index.txt \
+nslices=`fslinfo dwi_comb.nii.gz | grep -e "^dim3" | awk '{ print $2 }'`;
+let nslices=$nslices-1;  # make it zero based
+seq 1 2 $nslices > my_slspec.txt;
+seq 0 2 $nslices >> my_slspec.txt;
+
+eddy_cuda --imain=dwi_comb --acqp=acqparams.txt --index=index.txt \
     --mask=b0_brain_mask --bvals=dwi_bval.dat --bvecs=dwi_rvec.dat \
     --out=eddy_s2v_unwarped_images --niter=8 --fwhm=10,6,4,2,0,0,0,0 \
     --repol --ol_type=both --mporder=8 --s2v_niter=8 \
     --slspec=my_slspec.txt --cnr_maps
+
