bf60134fa770c66e35d51ac3ba8e76aa0449cd56
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-04-03 12:04:55 -0400

    wrapper to run tarckStructSubject locally

---
+++ dti/run_trackSubjectStruct.sh
@@ -0,0 +1,28 @@
+# Wrapper to run trackSubjectStruct all in /lscratch after CIT's freakout on the
+# usage of fsl_sub.
+# Usage: bash ~/research_code/dti/run_trackSubjectStruct maskid
+
+scan=$1
+
+dataDir=/data/NCR_SBRB/dti_fdt
+execPath=/data/NCR_SBRB/software/autoPtx
+structures=$execPath/structureList
+track=$execPath/trackSubjectStruct
+
+# copy all data we'll need first
+cd /lscratch/${SLURM_JOBID}/
+mkdir preproc;
+cp -r $dataDir/preproc/${scan}* preproc/;
+
+while read structstring; do
+    struct=`echo $structstring | awk '{print $1}'`
+    nseed=`echo $structstring | awk '{print $2}'`
+    echo $struct;
+    $track $scan $struct $nseed 2>&1;
+done < $structures
+
+# copy all results back
+if [ ! -d $dataDir/tracts ]; then
+    mkdir $dataDir/tracts;
+fi
+cp -r tracts/${scan} $dataDir/tracts/;
--- fmri/collect_3dNetCorr_grids.R
+++ fmri/collect_3dNetCorr_grids.R
@@ -5,42 +5,50 @@
 
 subj_dir = '~/data/heritability_change/fmri_corr_tables/'
 subjs = read.csv(sprintf('%s/../rsfmri_3min_assoc_n462.csv', subj_dir))[,'Mask.ID']
-expected_rois = 87
 
 library(plyr)  # for rbind.fill
 
-data = c()
-for (s in subjs) {
-    print(s)
-    a = read.table(sprintf('%s/%04d_aparc_000.netcc', subj_dir, s), header=1)
-    # remove weird integer row
-    b = a[2:nrow(a),]
-    # split matrix into first set of rows as Rs, second set as Zs
-    rs = b[1:ncol(b),]
-    zs = b[(ncol(b)+1):nrow(b),]
-    # put correct names in the square matrix
-    rois = colnames(rs)
-    rownames(rs) = rois
-    
-    # note that we're doing this only for Rs, but we could as easily do it for Zs!
-  
-    b.tri = rs[upper.tri(rs, diag=F)]
-    # format a header for the vectorized version of the table
-    w = which(upper.tri(rs, diag=F), arr.ind = TRUE)
-    header = sapply(1:length(b.tri),
-                    function(i) sprintf('%s.TO.%s', rois[w[i,1]],
-                                                    rois[w[i,2]]))
-    # concatenate vectorized matrices for the same mask id in the same row
-    subj_data = c(s, b.tri)
-    names(subj_data) = c('mask.id', header)  
-
-    if (length(rois) == expected_rois) {
-        data = rbind(data, subj_data)
+for (parc in c('aparc', 'aparc.a2009s')) {
+    print(parc)
+    if (parc == 'aparc') {
+        expected_rois = 87
     } else {
-        # this is much slower than regular bind, so we only do it when needed
-        print(sprintf('Different number of ROIs (%d): filling missing with NA', length(rois)))
-        data = rbind.fill(as.data.frame(data), as.data.frame(t(subj_data)))
+        expected_rois = 167
     }
+    data = c()
+    for (s in subjs) {
+        a = read.table(sprintf('%s/%04d_%s_000.netcc', subj_dir, s, parc),
+                       header=1)
+        # remove weird integer row
+        b = a[2:nrow(a),]
+        # split matrix into first set of rows as Rs, second set as Zs
+        rs = b[1:ncol(b),]
+        zs = b[(ncol(b)+1):nrow(b),]
+        # put correct names in the square matrix
+        rois = colnames(rs)
+        rownames(rs) = rois
+        
+        # note that we're doing this only for Rs, but we could as easily do it for Zs!
+    
+        b.tri = rs[upper.tri(rs, diag=F)]
+        # format a header for the vectorized version of the table
+        w = which(upper.tri(rs, diag=F), arr.ind = TRUE)
+        header = sapply(1:length(b.tri),
+                        function(i) sprintf('%s.TO.%s', rois[w[i,1]],
+                                                        rois[w[i,2]]))
+        # concatenate vectorized matrices for the same mask id in the same row
+        subj_data = c(s, b.tri)
+        names(subj_data) = c('mask.id', header)  
+
+        if (length(rois) == expected_rois) {
+            data = rbind(data, subj_data)
+        } else {
+            # this is much slower than regular bind, so we only do it when needed
+            print(sprintf('Scan %s has different number of ROIs (%d): filling missing with NA', s, length(rois)))
+            data = rbind.fill(as.data.frame(data), as.data.frame(t(subj_data)))
+        }
+    }
+    # rename it so that each row is a mask id
+    write.csv(data, file=sprintf('%s/pearson_3min_n462_%s.csv', subj_dir, parc),
+              row.names=F)
 }
-# rename it so that each row is a mask id
-write.csv(data, file=sprintf('%s/pearson_3min_n462_aparc.csv', subj_dir), row.names=F)
