1c138646a5c46c2b7563e840ba4845ebda43246d
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-04-03 13:40:12 -0400

    adding verbosity

--- dti/run_trackSubjectStruct.sh
+++ dti/run_trackSubjectStruct.sh
@@ -2,6 +2,8 @@
 # usage of fsl_sub.
 # Usage: bash ~/research_code/dti/run_trackSubjectStruct maskid
 
+module load fsl
+
 scan=$1
 
 dataDir=/data/NCR_SBRB/dti_fdt
@@ -18,11 +20,11 @@ while read structstring; do
     struct=`echo $structstring | awk '{print $1}'`
     nseed=`echo $structstring | awk '{print $2}'`
     echo $struct;
-    $track $scan $struct $nseed 2>&1 > /dev/null;
+    $track $scan $struct $nseed 2>&1 > ${scan}.log;
 done < $structures
 
 # copy all results back
 if [ ! -d $dataDir/tracts ]; then
     mkdir $dataDir/tracts;
 fi
-cp -r tracts/${scan} $dataDir/tracts/;
+cp -r tracts/${scan} ${scan}.log $dataDir/tracts/;
