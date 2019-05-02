324514d135e4aa72329f481cb788d3d79b9260fb
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-05-02 16:46:50 -0400

    playing with solar voxel again

--- run_solar_voxel.sh
+++ run_solar_voxel.sh
@@ -2,13 +2,13 @@
 phen_file=$1
 v=$2
 tmp_dir=~/data/tmp/
-solar_dir=~/data/solar_paper_noMeds/
+solar_dir=~/data/heritability_change/
 vox=`printf "%05d" $v`
 mkdir ${tmp_dir}/${phen_file}
 mkdir /lscratch/${SLURM_JOBID}/${phen_file}
 mkdir /lscratch/${SLURM_JOBID}/${phen_file}/${vox}
 cp ${solar_dir}/pedigree.csv ${solar_dir}/procs.tcl ${solar_dir}/${phen_file}.csv /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/
 cd /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/
-solar run_ica_voxel $phen_file $v
+solar run_phen_var $phen_file $v
 mv /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/i_v${v}/polygenic.out ${tmp_dir}/${phen_file}/v${vox}_polygenic.out
 rm -rf /lscratch/${SLURM_JOBID}/${phen_file}/${vox}
