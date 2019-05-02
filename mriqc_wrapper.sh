73adc5a7e740e4eb50738d3141f58ba93d1fb292
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-05-02 13:26:36 -0400

    setting memory and threading limits

--- mriqc_wrapper.sh
+++ mriqc_wrapper.sh
@@ -4,7 +4,7 @@ s=$1;
 TMPDIR=/lscratch/$SLURM_JOBID; 
 mkdir -p $TMPDIR/out; 
 mkdir -p $TMPDIR/wrk;
-mriqc /scratch/${USER}/NCR_BIDS/ $TMPDIR/out \
-    participant --participant_label ${s} --no-sub -w $TMPDIR/wrk;
-mv $TMPDIR/out/subj-${s}* /data/${USER}/mriqc_output/
+mriqc /scratch/${USER}/NCR_BIDS/ $TMPDIR/out participant \
+    --participant_label ${s} --no-sub --n_procs 4 --mem_gb 8 -w $TMPDIR/wrk;
+mv $TMPDIR/out/sub-${s}* /data/${USER}/mriqc_output/
 
