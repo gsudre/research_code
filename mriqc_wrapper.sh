f78e3a41ccc8eb0d1aef4a9e609498230a802ef7
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-05-02 11:13:46 -0400

    better handling naming

--- mriqc_wrapper.sh
+++ mriqc_wrapper.sh
@@ -1,9 +1,10 @@
-# Runs mriqc using lscratch
+# Runs mriqc using lscratch. The only parameter is the mask id!
+
 s=$1;
 TMPDIR=/lscratch/$SLURM_JOBID; 
 mkdir -p $TMPDIR/out; 
 mkdir -p $TMPDIR/wrk;
 mriqc /scratch/${USER}/NCR_BIDS/ $TMPDIR/out \
     participant --participant_label ${s} -w $TMPDIR/wrk;
-mv $TMPDIR/out/${s}* /data/${USER}/mriqc_output/
+mv $TMPDIR/out/subj-${s}* /data/${USER}/mriqc_output/
 
