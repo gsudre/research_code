28464f2a2d68a91f7ae88cad36d01ac04620bda3
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-05-02 11:21:45 -0400

    adding no sub

--- mriqc_wrapper.sh
+++ mriqc_wrapper.sh
@@ -5,6 +5,6 @@ TMPDIR=/lscratch/$SLURM_JOBID;
 mkdir -p $TMPDIR/out; 
 mkdir -p $TMPDIR/wrk;
 mriqc /scratch/${USER}/NCR_BIDS/ $TMPDIR/out \
-    participant --participant_label ${s} -w $TMPDIR/wrk;
+    participant --participant_label ${s} --no-sub -w $TMPDIR/wrk;
 mv $TMPDIR/out/subj-${s}* /data/${USER}/mriqc_output/
 
