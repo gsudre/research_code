f698511197821de7c76a486298b557ad471579b8
Gustavo Sudre <gustavo.sudre@nih.gov>
2019-05-03 13:56:27 -0400

    fixing variables

--- run_solar_voxel.sh
+++ run_solar_voxel.sh
@@ -10,6 +10,6 @@ mkdir /lscratch/${SLURM_JOBID}/${phen_file}/${vox}
 cp ${solar_dir}/pedigree.csv ${solar_dir}/procs.tcl /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/
 sed "s/NA//g" ${solar_dir}/${phen_file}.csv > /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/${phen_file}.csv;
 cd /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/
-solar run_phen_var $phen_file $v
+solar run_phen_var $phen_file $vox
 mv /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/i_v${v}/polygenic.out ${tmp_dir}/${phen_file}/v${vox}_polygenic.out
 rm -rf /lscratch/${SLURM_JOBID}/${phen_file}/${vox}
