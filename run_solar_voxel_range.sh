0b2e7f05777dbe157830d43bf05bb8c18056d434
Sudre <sudregp@hg-02113362-dm4.nih.gov>
2019-05-24 13:08:31 -0400

    implemented wolfgang's recommendations

--- run_solar_voxel_range.sh
+++ run_solar_voxel_range.sh
@@ -1,25 +1,31 @@
-# we have to create separate solar directories, so the SOLAR residual files don't get overwritten as we run multiple voxels in parallel
 phen_file=$1
 v1=$2
 v2=$3
 tmp_dir=~/data/tmp/
-solar_dir=~/data/heritability_change/
+
+solar_dir=/data/sudregp/heritability_change/
+
 mkdir ${tmp_dir}/${phen_file}
 mkdir /lscratch/${SLURM_JOBID}/${phen_file}
 # making sure we only access the network once
 cp ${solar_dir}/pedigree.csv ${solar_dir}/procs.tcl /lscratch/${SLURM_JOBID}/${phen_file}/;
 sed "s/NA//g" ${solar_dir}/perms/${phen_file}.csv > /lscratch/${SLURM_JOBID}/${phen_file}/${phen_file}.csv;
 for v in `seq $v1 $v2`; do
-    vox=`printf "%06d" $v`;
-    mkdir /lscratch/${SLURM_JOBID}/${phen_file}/${vox};
-    cd /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/;
-    cp ../pedigree.csv ../procs.tcl ../${phen_file}.csv ./;
-    solar run_phen_var $phen_file v${vox};
-    mv i_v${vox}/polygenic.out ../v${vox}_polygenic.out;
-    # prevents local directory from growing unnecessarily
-    cd ..;
-    rm -rf ${vox};
+     vox=`printf "%06d" $v`;
+     mkdir /lscratch/${SLURM_JOBID}/${phen_file}/${vox};
+     cd /lscratch/${SLURM_JOBID}/${phen_file}/${vox}/;
+
+     ln ../pedigree.csv ../procs.tcl ../${phen_file}.csv .
+
+     solar run_phen_var $phen_file v${vox};
+     mv i_v${vox}/polygenic.out ../v${vox}_polygenic.out;
+     # prevents local directory from growing unnecessarily
+     cd ..;
+     rm -rf ${vox};
 done;
-cp v*_polygenic.out ${tmp_dir}/${phen_file}/;
+
+tar -czf ${phen_file}_${v1}to${v2}.tgz v*_polygenic.out
+cp ${phen_file}_${v1}to${v2}.tgz ${tmp_dir}/${phen_file}/
+
 cd /lscratch/${SLURM_JOBID};
 rm -rf /lscratch/${SLURM_JOBID}/${phen_file};
