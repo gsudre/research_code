aaa9d69c4a747eb50268b6a3c44f2d9a6291b613
Sudre <sudregp@hg-02113362-dm4.nih.gov>
2019-06-27 16:05:27 -0400

    scripted function to create aroma slopes

---
+++ fmri/create_aroma_slopes.R
@@ -0,0 +1,113 @@
+args <- commandArgs(trailingOnly = TRUE)
+pipe = args[1]
+tmin = as.numeric(args[2])
+
+source('~/research_code/lab_mgmt/merge_on_closest_date.R')
+fname = sprintf('~/data/heritability_change/rsfmri_AROMA%s_%dmin_best2scans.csv', pipe, tmin)
+m = read.csv(fname)
+df_var_names = colnames(m)[!grepl(colnames(m), pattern="conn")]
+clin = read.csv('~/data/heritability_change/clinical_06262019.csv')
+df = mergeOnClosestDate(m[, df_var_names], clin, unique(m$Medical.Record...MRN),
+                         x.date='record.date.collected...Scan',
+                         x.id='Medical.Record...MRN')
+brain_var_names = colnames(m)[grepl(colnames(m), pattern="conn")]
+df2 = merge(df, m[, c('Mask.ID', brain_var_names)], by='Mask.ID', all.x=F)
+
+# make sure we still have two scans for everyone
+rm_subjs = names(which(table(df2$Medical.Record...MRN)<2))
+rm_me = df2$Medical.Record...MRN %in% rm_subjs
+df2 = df2[!rm_me, ]
+
+mres = df2
+mres$SX_HI = as.numeric(as.character(mres$SX_hi))
+mres$SX_inatt = as.numeric(as.character(mres$SX_inatt))
+
+res = c()
+for (s in unique(mres$Medical.Record...MRN)) {
+    idx = which(mres$Medical.Record...MRN == s)
+    row = c(s, unique(mres[idx, 'Sex']))
+    phen_cols = c(brain_var_names, 'SX_inatt', 'SX_HI')
+    y = mres[idx[2], phen_cols] - mres[idx[1], phen_cols]
+    x = mres[idx[2], 'age_at_scan'] - mres[idx[1], 'age_at_scan']
+    slopes = y / x
+    row = c(row, slopes)
+
+    # grabbing inatt and HI at baseline
+    base_DOA = which.min(mres[idx, 'age_at_scan'])
+    row = c(row, mres[idx[base_DOA], 'SX_inatt'])
+    row = c(row, mres[idx[base_DOA], 'SX_HI'])
+    # DX1 is DSMV definition, DX2 will make SX >=4 as ADHD
+    if (mres[idx[base_DOA], 'age_at_scan'] < 16) {
+        if ((row[length(row)] >= 6) || (row[length(row)-1] >= 6)) {
+            DX = 'ADHD'
+        } else {
+            DX = 'NV'
+        }
+    } else {
+        if ((row[length(row)] >= 5) || (row[length(row)-1] >= 5)) {
+            DX = 'ADHD'
+        } else {
+            DX = 'NV'
+        }
+    }
+    if ((row[length(row)] >= 4) || (row[length(row)-1] >= 4)) {
+        DX2 = 'ADHD'
+    } else {
+        DX2 = 'NV'
+    }
+    row = c(row, DX)
+    row = c(row, DX2)
+    res = rbind(res, row)
+    print(nrow(res))
+}
+colnames(res) = c('ID', 'sex', brain_var_names, c('SX_inatt', 'SX_HI',
+                                              'inatt_baseline',
+                                              'HI_baseline', 'DX', 'DX2'))
+# we only open this in R, so it's OK to be RData to load faster
+fname = sprintf('~/data/heritability_change/rsfmri_AROMA%s_%dmin_best2scansSlopes_n%d.RData',
+                        pipe, tmin, nrow(res))
+save(res, file=fname)
+
+# and remove outliers
+res_clean = res
+for (t in brain_var_names) {
+    mydata = as.numeric(res_clean[, t])
+    # identifying outliers
+    ul = mean(mydata) + 3 * sd(mydata)
+    ll = mean(mydata) - 3 * sd(mydata)
+    bad_subjs = c(which(mydata < ll), which(mydata > ul))
+
+    # remove within-variable outliers
+    res_clean[bad_subjs, t] = NA
+}
+fname = sprintf('~/data/heritability_change/rsfmri_AROMA%s_%dmin_best2scansSlopesClean_n%d.RData',
+                        pipe, tmin, nrow(res_clean))
+save(res_clean, file=fname)
+
+# and make sure every family has at least two people
+good_nuclear = names(table(m$Nuclear.ID...FamilyIDs))[table(m$Nuclear.ID...FamilyIDs) >= 4]
+good_extended = names(table(m$Extended.ID...FamilyIDs))[table(m$Extended.ID...FamilyIDs) >= 4]
+keep_me = c()
+for (f in good_nuclear) {
+    keep_me = c(keep_me, m[which(m$Nuclear.ID...FamilyIDs == f),
+                            'Medical.Record...MRN'])
+}
+for (f in good_extended) {
+    keep_me = c(keep_me, m[which(m$Extended.ID...FamilyIDs == f),
+                            'Medical.Record...MRN'])
+}
+keep_me = unique(keep_me)
+
+fam_subjs = c()
+for (s in keep_me) {
+    fam_subjs = c(fam_subjs, which(res[, 'ID'] == s))
+}
+res2 = res[fam_subjs, ]
+res2_clean = res_clean[fam_subjs, ]
+
+fname = sprintf('~/data/heritability_change/rsfmri_AROMA%s_%dmin_best2scansFamsSlopes_n%d.csv',
+                        pipe, tmin, nrow(res2))
+write.csv(res2, file=fname, row.names=F, na='', quote=F)
+fname = sprintf('~/data/heritability_change/rsfmri_AROMA%s_%dmin_best2scansFamsSlopesClean_n%d.csv',
+                        pipe, tmin, nrow(res2_clean))
+write.csv(res2_clean, file=fname, row.names=F, na='', quote=F)
