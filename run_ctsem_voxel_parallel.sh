6efa02571642389a0eb0a23e4cfc50e148be7d2a
Sudre <sudregp@hg-02113362-dm4.nih.gov>
2019-07-09 13:19:39 -0400

    fixing variable names

--- run_ctsem_voxel_parallel.sh
+++ run_ctsem_voxel_parallel.sh
@@ -15,11 +15,11 @@ for v in `seq $v1 $v2`; do
     echo Y${v} >> var_list.txt;
 done
 split -n $SLURM_CPUS_PER_TASK var_list.txt;
-fbase=`basename -s RData.gz $data_file`;
+fbase=`basename -s .RData.gz $data_file`;
 
-ls -1 xa? > file_list.txt;
+ls -1 x?? > file_list.txt;
 cat file_list.txt | parallel -j $SLURM_CPUS_PER_TASK --max-args=1 \
-    Rscript ctsem_voxel_developmental_time_3_timepoints.R \
+    Rscript ~/research_code/ctsem_voxel_developmental_time_3_timepoints.R \
         `basename $data_file` \
         $sx {} ${tmp_dir}/tmp/${fbase}_${sx}_${v1}to${v2}_{}.csv;
 
