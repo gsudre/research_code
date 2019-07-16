708a971392ed281010aedbe736057ac990440cdf
Sudre <sudregp@hg-02113362-dm4.nih.gov>
2019-07-16 13:43:59 -0400

    added permuted data

--- ctsem_voxel_developmental_time_3_timepoints.R
+++ ctsem_voxel_developmental_time_3_timepoints.R
@@ -1,7 +1,7 @@
 # Runs ctsem for voxels
 #
-# Usage: Rscript ctsem_loop.R data_fname sx1 phen_list_file out_fname
-# 
+# Usage: Rscript ctsem_loop.R data_fname sx1 phen_list_file out_fname 0
+# (use > 0 as the last argument for permutations)
 # GS. 07/2019
 library(ctsem)
 
@@ -10,6 +10,11 @@ data_fname = args[1]
 s = args[2]
 phen_fname = args[3]
 out_fname = args[4]
+if (length(args) == 5) {
+    perm_seed = as.numeric(args[5])
+} else {
+    perm_seed = 0
+}
 
 TIs = c('TI1')
 # TIs = c('TI1', 'TI2')
@@ -18,10 +23,21 @@ TIs = c('TI1')
 
 load(data_fname)
 phen = as.character(read.table(phen_fname)[,1])
+
+if (perm_seed > 0) {
+    # let's permute just the symptom columns
+    print('Permuting data!!!')
+    set.seed(perm_seed)
+    rnd_idx = sample.int(nrow(wider_resids_motion))
+    s_cols = c(sprintf('%s_T0', s), sprintf('%s_T1', s),
+               sprintf('%s_T2', s), sprintf('%s_T3', s))
+    wider_resids_motion[, s_cols]  wider_resids_motion[rnd_idx, s_cols]
+}
+
 out_res = c()
 for (p in phen) {
     set.seed(42)
-    
+
     cat(sprintf('Running %s and %s\n', s, p))
     myctmodel = ctModel(n.latent=2, n.manifest=2, Tpoints=4, n.TIpred=length(TIs),
                         manifestNames=c(s, p), latentNames=c(s, p),
