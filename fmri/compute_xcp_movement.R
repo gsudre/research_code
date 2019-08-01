3eaafc97df05e97c2180b51fd9afd33c763beee2
Sudre <sudregp@hg-01881717-lm3.nih.gov>
2019-08-01 17:41:41 -0400

    changing calculation of meanFD

--- fmri/compute_xcp_movement.R
+++ fmri/compute_xcp_movement.R
@@ -1,15 +1,20 @@
 # calculates TRs left, mean FD and meanDVARS for only the TRs used in each
 # pipeline
  
-pipes = c('', '-p5', '-p5-nc', '-p25', '-p25-nc',
-          '-gsr', '-gsr-p5', '-gsr-p5-nc', '-gsr-p25', '-gsr-p25-nc')
-pipes = c('fc-36p_despike', 'fc-36p', 'fc-36p_scrub_p25', 'fc-36p_scrub_p5',
+# pipes = c('', '-p5', '-p5-nc', '-p25', '-p25-nc',
+#           '-gsr', '-gsr-p5', '-gsr-p5-nc', '-gsr-p25', '-gsr-p25-nc')
+# pipes = c('fc-36p_despike', 'fc-36p', 'fc-36p_scrub_p25', 'fc-36p_scrub_p5',
+#           'fc-36p_spkreg')
+
+pipes = c('AROMA', 'AROMA-p5', 'AROMA-p5-nc', 'AROMA-p25', 'AROMA-p25-nc',
+          'AROMA-gsr', 'AROMA-gsr-p5', 'AROMA-gsr-p5-nc',
+          'AROMA-gsr-p25', 'AROMA-gsr-p25-nc',
+          'fc-36p_despike', 'fc-36p', 'fc-36p_scrub_p25', 'fc-36p_scrub_p5',
           'fc-36p_spkreg')
 
 # grab all subjects
 all_subjs = c()
 for (p in pipes) {
-    # mydir = sprintf('/Volumes/Shaw/AROMA_ICA/xcpengine_output_AROMA%s', p)
     mydir = sprintf('/data/NCR_SBRB/xcpengine_output_%s', p)
     cat(sprintf('Gathering subjects from %s\n', mydir))
     subjs = list.files(path=mydir)
@@ -19,7 +24,7 @@ all_subjs = all_subjs[grepl(all_subjs, pattern='^sub-')]
 all_subjs = unique(all_subjs)
 
 mycols = c('subj', 'pipeline', 'TRs_used', 'meanFD', 'meanDVARS',
-           'fcon')
+           'power264')
 res = matrix(nrow=(length(all_subjs) * length(pipes)), ncol=length(mycols))
 colnames(res) = mycols
 
@@ -29,21 +34,28 @@ for (s in all_subjs) {
     for (p in pipes) {
         res[r, 'subj'] = s
         res[r, 'pipeline'] = p
-        # mydir = sprintf('/Volumes/Shaw/AROMA_ICA/xcpengine_output_AROMA%s/%s',
-        #                 p, s)
         mydir = sprintf('/data/NCR_SBRB/xcpengine_output_%s/%s', p, s)
         fname = sprintf('%s/%s-nFlags.1D', mydir, s)
         if (file.exists(fname)) {
             censored = read.table(fname)[, 1]
-            # when there's no censoring, nFlags is weird
-            if (length(censored) == 1) {
-                censored = rep(0, 126)
-            }
-            good_TRs = censored == 0
-            res[r, 'TRs_used'] = sum(good_TRs)
             fname = sprintf('%s/prestats/%s_fmriconf.tsv', mydir, s)
             if (file.exists(fname)) {
                 data = read.table(fname, header=1)
+
+                # for some pipelines that don't need censoring xcpengine still
+                # makes a healthy nFlags file, so we need to identify those
+                # pipelines and skip their nFlags!
+                orig_TRs = nrow(data)
+                fname = sprintf('%s/qcfc/%s_dvars-raw.1D', mydir, s)[, 1]
+                junk = read.table(fname)
+                used_TRs = length(junk)
+                if (used_TRs == orig_TRs) {
+                    censored = rep(0, orig_TRs)
+                }
+                
+                good_TRs = censored == 0
+                res[r, 'TRs_used'] = sum(good_TRs)
+                
                 # massaging to avoid first NA
                 tmp = as.character(data[good_TRs, 'framewise_displacement'])
                 res[r, 'meanFD'] = mean(as.numeric(tmp[2:length(tmp)]),
