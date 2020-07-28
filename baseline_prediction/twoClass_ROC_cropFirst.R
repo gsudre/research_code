a9f46f26947db4df4007356b372fe868c1d865f5
Gustavo Sudre <gustavo.sudre@nih.gov>
2020-07-27 22:05:22 -0400

    lots of testing scripts

--- baseline_prediction/twoClass_ROC_cropFirst.R
+++ baseline_prediction/twoClass_ROC_cropFirst.R
@@ -1,8 +1,6 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 library(caret)
-library(caretEnsemble)
-library(doParallel)
 
 if (length(args) > 0) {
     fname = args[1]
@@ -11,55 +9,50 @@ if (length(args) > 0) {
     c2 = args[4]
     clf_model = args[5]
     impute = args[6]
-    nfolds = as.numeric(args[7])
-    nreps = as.numeric(args[8])
-    ncores = as.numeric(args[9])
-    use_covs = as.logical(args[10])
-    out_file = args[11]
+    use_covs = as.logical(args[7])
+    out_file = args[8]
 } else {
-    fname = '~/data/baseline_prediction/gf_JULY_ols_definition_GS.csv'
+    fname = '~/data/baseline_prediction/FINAL_DATA_to_gs_JULY_26_2020b.csv'
     phen = 'categ_all_lm'
-    c1 = 'never_affected'
+    c1 = 'worsening'
     c2 = 'stable'
-    clf_model = 'cforest'
+    clf_model = 'slda'
     impute = 'dti'
-    nfolds = 10
-    nreps = 10
-    ncores = 2
     use_covs = FALSE
     out_file = '/dev/null'
 }
 
 data = read.csv(fname)
 data$sex_numeric = as.factor(data$sex_numeric)
-data$SES_group3 = as.factor(data$SES_group3)
+# data$SES_group3 = as.factor(data$SES_group3)
 data$base_total = data$base_inatt + data$base_hi
-# data$slf_fa = data$slf_all  # just to make it easier to filter out
-var_names = c(# PRS
-              'ADHD_PRS0.000100.orig', 'ADHD_PRS0.001000.orig',
-              'ADHD_PRS0.010000.orig', 'ADHD_PRS0.050000.orig',
-              'ADHD_PRS0.100000.orig', 'ADHD_PRS0.200000.orig',
-              'ADHD_PRS0.300000.orig', 'ADHD_PRS0.400000.orig',
-              'ADHD_PRS0.500000.orig',
+
+var_names = c(
+              # PRS
+              'ADHD_PRS0.000100', 'ADHD_PRS0.001000',
+              'ADHD_PRS0.010000', 'ADHD_PRS0.050000',
+              'ADHD_PRS0.100000', 'ADHD_PRS0.200000',
+              'ADHD_PRS0.300000', 'ADHD_PRS0.400000',
+              'ADHD_PRS0.500000',
               # DTI
               'atr_fa', 'cst_fa', 'cing_cing_fa', 'cing_hipp_fa', 'cc_fa',
-              'ilf_fa', 'slf_fa', 'unc_fa',
-            #   demo
-              'sex_numeric', 'base_age',# 'SES_group3', 
+              'ilf_fa', 'slf_fa', 'unc_fa', 'ifof_fa',
+              #   demo
+              'sex_numeric', 'base_age',
                 # 'SES_group3',
               # cog
-              'FSIQ', 'SS_RAW', 'DS_RAW', 'PS_RAW', 'VMI.beery_RAW',
+              'FSIQ', 'SS.wisc', 'DS.wisc', 'PS', 'VMI.beery',
             #   # anat
-              'cerebellum_white', 'cerebellum_grey', 'amygdala',
+              'cerbellum_white', 'cerebllum_grey', 'amygdala',
               'cingulate', 'lateral_PFC', 'OFC', 'striatum', 'thalamus'
             #   # base SX
-            #   'base_inatt', 'base_hi',
-            #   'slf_fa'
+            #   'base_inatt', 'base_hi'
             # 'age_onset'
+            # 'last_age'
               )
 
 covar_names = c(# DTI
-                'norm.rot', 'norm.trans', # base_age, gender,
+                'norm.rot', 'norm.trans',
                 # cog
                 # 'base_age', 'sex_numeric',
                 # anat
@@ -76,19 +69,19 @@ if (use_covs) {
     data2 = data[, var_names]
 }
 
-# data2 = data2[data$ever_ADHD == 'include',]
-# data = data[data$ever_ADHD == 'include',]
+# data2 = data2[data$EVER_ADHD == 'yes',]
+# data = data[data$EVER_ADHD == 'yes',]
 
-# data2 = data2[data$parent_history_include == 'yes',]
-# data = data[data$parent_history_include == 'yes',]
+# data2 = data2[data$include_parental_history == 'yes',]
+# data = data[data$include_parental_history == 'yes',]
 
 if (impute == 'dti') {
-    use_me = !is.na(data2$slf_fa)
+    use_me = !is.na(data$slf_fa)
     data2 = data2[use_me, ]
     print(sprintf('Using %d observations, %d predictors.', nrow(data2),
                   ncol(data2))) 
 } else if (impute == 'anat') {
-    use_me = !is.na(data2$thalamus)
+    use_me = !is.na(data$thalamus)
     data2 = data2[use_me, ]
     dti_cols = which(grepl(colnames(data2), pattern='_fa$'))
     data2 = data2[, -dti_cols]
@@ -103,16 +96,19 @@ if (impute == 'dti') {
     print('No imputation needed')
 }
 
-# data2$slf_fa = NULL
-
 # will need this later so training rows match data2
 data = data[use_me, ]
+
+dummies = dummyVars( ~ ., data = data2)
+data3 = predict(dummies, newdata = data2)
+
 data2$phen = as.factor(data[, phen])
 
-
-
-dummies = dummyVars(phen ~ ., data = data2)
-data3 = predict(dummies, newdata = data2)
+# selecting only kids in the 2 specified groups
+keep_me = data2$phen==c1 | data2$phen==c2
+data3 = data3[keep_me, ]
+data2 = data2[keep_me, ]
+data = data[keep_me, ]
 
 # split traing and test between members of the same family
 train_rows = c()
@@ -121,24 +117,15 @@ for (fam in unique(data$FAMID)) {
     if (length(fam_rows) == 1) {
         train_rows = c(train_rows, fam_rows[1])
     } else {
-        # choose the oldest kid in the family for training
         train_rows = c(train_rows,
-                       fam_rows[which.max(data[fam_rows, 'base_age'])])
+                       fam_rows[which.min(data[fam_rows, 'base_age'])])
     }
 }
 # data3 doesn't have the target column!
 X_train <- as.data.frame(data3[train_rows, ])
 X_test <- as.data.frame(data3[-train_rows, ])
-y_train <- data2[train_rows,]$phen
-y_test <- data2[-train_rows,]$phen
-
-# selecting only kids in the 2 specified groups
-keep_me = y_train==c1 | y_train==c2
-X_train = as.data.frame(X_train[keep_me, ])
-y_train = factor(y_train[keep_me])
-keep_me = y_test==c1 | y_test==c2
-X_test = as.data.frame(X_test[keep_me, ])
-y_test = factor(y_test[keep_me])
+y_train <- factor(data2[train_rows,]$phen)
+y_test <- factor(data2[-train_rows,]$phen)
 
 # imputation and feature engineering
 set.seed(42)
@@ -158,38 +145,17 @@ print(colnames(X_train))
 print(table(y_train))
 print(table(y_test))
 
-registerDoParallel(ncores)
-getDoParWorkers()
 set.seed(42)
-
-fitControl <- trainControl(method = "repeatedcv",
-                           number = nfolds,
-                           repeats = nreps,
-                           savePredictions = 'final',
-                           allowParallel = TRUE,
+fitControl <- trainControl(method = "none",
                            classProbs = TRUE,
                            summaryFunction=twoClassSummary)
 
 set.seed(42)
-model_list <- caretList(X_train,
+fit <- train(X_train,
                         y_train,
                         trControl = fitControl,
-                        methodList = c(clf_model, 'null'),
-                        tuneList = NULL,
-                        continue_on_fail = TRUE,
+                        method = clf_model,
                         metric='ROC')
-
-fit = model_list[[clf_model]]
-resamps = resamples(list(fit=fit, tmp=fit))
-auc_stats = summary(resamps)$statistics$ROC['fit',]
-cnames = sapply(names(auc_stats), function(x) sprintf('AUC_%s', x))
-names(auc_stats) = cnames
-sens_stats = summary(resamps)$statistics$Sens['fit',]
-cnames = sapply(names(sens_stats), function(x) sprintf('Sens_%s', x))
-names(sens_stats) = cnames
-spec_stats = summary(resamps)$statistics$Spec['fit',]
-cnames = sapply(names(spec_stats), function(x) sprintf('Spec_%s', x))
-names(spec_stats) = cnames
 
 preds_class = predict.train(fit, newdata=X_test)
 preds_probs = predict.train(fit, newdata=X_test, type='prob')
@@ -198,11 +164,10 @@ mcs = twoClassSummary(dat, lev=colnames(preds_probs))
 test_results = c(mcs['ROC'], mcs['Sens'], mcs['Spec'])
 names(test_results) = c('test_AUC', 'test_Sens', 'test_Spec')
 
-res = c(phen, clf_model, c1, c2, impute, use_covs, nfolds, nreps,
-        auc_stats, sens_stats, spec_stats, test_results)
+res = c(phen, clf_model, c1, c2, impute, use_covs,
+        test_results, table(y_train), table(y_test))
 line_res = paste(res,collapse=',')
 write(line_res, file=out_file, append=TRUE)
-print(line_res)
 
 # export variable importance
 a = varImp(fit, useModel=T)
@@ -210,14 +175,16 @@ b = varImp(fit, useModel=F)
 split_fname = strsplit(x=out_file, split='/')[[1]]
 myprefix = gsub(x=split_fname[length(split_fname)], pattern='.csv', replacement='')
 out_dir = '~/data/baseline_prediction/more_models/'
-fname = sprintf('%s/varimp_%s_%s_%s_%s_%s_%s_%s_%d_%d.csv',
-                out_dir, myprefix, clf_model, phen, c1, c2, impute, use_covs, nfolds, nreps)
+fname = sprintf('%s/varimp_%s_%s_%s_%s_%s_%s_%s.csv',
+                out_dir, myprefix, clf_model, phen, c1, c2, impute, use_covs)
 # careful here because for non-linear models the rows of the importance matrix
 # are not aligned!!!
 write.csv(cbind(a$importance, b$importance), file=fname)
 print(varImp(fit, useModel=F, scale=F))
 
 # export fit
-fname = sprintf('%s/fit_%s_%s_%s_%s_%s_%s_%s_%d_%d.RData',
-                out_dir, myprefix, clf_model, phen, c1, c2, impute, use_covs, nfolds, nreps)
+fname = sprintf('%s/fit_%s_%s_%s_%s_%s_%s_%s.RData',
+                out_dir, myprefix, clf_model, phen, c1, c2, impute, use_covs)
 save(fit, file=fname)
+
+print(line_res)
---
+++ baseline_prediction/twoClass_ROC_experiments3.R
@@ -0,0 +1,207 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+library(caret)
+
+if (length(args) > 0) {
+    fname = args[1]
+    phen = args[2]
+    c1 = args[3]
+    c2 = args[4]
+    clf_model = args[5]
+    impute = args[6]
+    use_covs = as.logical(args[7])
+    out_file = args[8]
+} else {
+    fname = '~/data/baseline_prediction/FINAL_DATA_to_gs_JULY_26_2020b.csv'
+    phen = 'categ_all_lm'
+    c1 = 'stable'
+    clf_model = 'slda'
+    impute = 'dti'
+    use_covs = FALSE
+    out_file = '/dev/null'
+}
+
+data = read.csv(fname)
+data$sex_numeric = as.factor(data$sex_numeric)
+# data$SES_group3 = as.factor(data$SES_group3)
+data$base_total = data$base_inatt + data$base_hi
+
+var_names = c(
+              # PRS
+              'ADHD_PRS0.000100', 'ADHD_PRS0.001000',
+              'ADHD_PRS0.010000', 'ADHD_PRS0.050000',
+              'ADHD_PRS0.100000', 'ADHD_PRS0.200000',
+              'ADHD_PRS0.300000', 'ADHD_PRS0.400000',
+              'ADHD_PRS0.500000',
+              # DTI
+              'atr_fa', 'cst_fa', 'cing_cing_fa', 'cing_hipp_fa', 'cc_fa',
+              'ilf_fa', 'slf_fa', 'unc_fa', 'ifof_fa',
+              #   demo
+              'sex_numeric', 'base_age',
+                # 'SES_group3',
+              # cog
+              'FSIQ', 'SS.wisc', 'DS.wisc', 'PS', 'VMI.beery',
+            #   # anat
+              'cerbellum_white', 'cerebllum_grey', 'amygdala',
+              'cingulate', 'lateral_PFC', 'OFC', 'striatum', 'thalamus',
+            #   # base SX
+            #   'base_inatt', 'base_hi'
+            # 'age_onset'
+            'last_age'
+              )
+
+covar_names = c(# DTI
+                'norm.rot', 'norm.trans',
+                # cog
+                # 'base_age', 'sex_numeric',
+                # anat
+                'average_qc', # age, gender (but not ICV)
+                # PRS
+                sapply(1:10, function(x) sprintf('PC%02d', x)) # age, gender
+                # demo
+                # just base_age (as gender one was of the targets)
+                )
+
+if (use_covs) {
+    data2 = data[, c(var_names, covar_names)]
+} else {
+    data2 = data[, var_names]
+}
+
+# data2 = data2[data$EVER_ADHD == 'yes',]
+# data = data[data$EVER_ADHD == 'yes',]
+
+# data2 = data2[data$include_parental_history == 'yes',]
+# data = data[data$include_parental_history == 'yes',]
+
+if (impute == 'dti') {
+    use_me = !is.na(data$slf_fa)
+    data2 = data2[use_me, ]
+    print(sprintf('Using %d observations, %d predictors.', nrow(data2),
+                  ncol(data2))) 
+} else if (impute == 'anat') {
+    use_me = !is.na(data$thalamus)
+    data2 = data2[use_me, ]
+    dti_cols = which(grepl(colnames(data2), pattern='_fa$'))
+    data2 = data2[, -dti_cols]
+    if (use_covs) {
+        dti_cols = which(grepl(colnames(data2), pattern='^norm'))
+        data2 = data2[, -dti_cols]
+    }
+    print(sprintf('Using %d observations, %d predictors.', nrow(data2),
+                  ncol(data2)))
+} else {
+    use_me = TRUE
+    print('No imputation needed')
+}
+
+# will need this later so training rows match data2
+data = data[use_me, ]
+
+dummies = dummyVars( ~ ., data = data2)
+data3 = predict(dummies, newdata = data2)
+
+data2$phen = as.factor(data[, phen])
+
+# split traing and test between members of the same family
+train_rows = c()
+for (fam in unique(data$FAMID)) {
+    fam_rows = which(data$FAMID == fam)
+    if (length(fam_rows) == 1) {
+        train_rows = c(train_rows, fam_rows[1])
+    } else {
+        # choose the oldest kid in the family for training
+        train_rows = c(train_rows,
+                       fam_rows[which.max(data[fam_rows, 'base_age'])])
+    }
+}
+# data3 doesn't have the target column!
+X_train <- as.data.frame(data3[train_rows, ])
+X_test <- as.data.frame(data3[-train_rows, ])
+y_train <- as.character(data2[train_rows,]$phen)
+y_test <- as.character(data2[-train_rows,]$phen)
+
+# selecting only kids in the 2 specified groups
+keep_me = y_train==c1
+y_train[!keep_me] = 'others'
+y_train = factor(y_train)
+keep_me = y_test==c1
+y_test[!keep_me] = 'others'
+y_test = factor(y_test, lev=levels(y_train))
+
+# imputation and feature engineering
+set.seed(42)
+pp_order = c('zv', 'nzv', 'corr', 'YeoJohnson', 'center', 'scale', 'bagImpute')
+pp = preProcess(X_train, method = pp_order)
+X_train = predict(pp, X_train)
+X_test = predict(pp, X_test)
+
+# remove linear combination variables
+comboInfo <- findLinearCombos(X_train)
+if (length(comboInfo$remove) > 0) {
+    X_train = X_train[, -comboInfo$remove]
+    X_test = X_test[, -comboInfo$remove]
+}
+
+print(colnames(X_train))
+print(table(y_train))
+print(table(y_test))
+
+set.seed(42)
+up_train <- upSample(x = X_train, y = y_train) 
+X_train = up_train
+X_train$Class = NULL
+y_train = up_train$Class
+
+set.seed(42)
+fitControl <- trainControl(method = "none",
+                           classProbs = TRUE,
+                           summaryFunction=twoClassSummary)
+
+idx = y_train==c1
+pvals = sapply(1:ncol(X_train),
+                function(x) {t.test(X_train[idx, x],
+                                        X_train[!idx, x])$p.value})
+good_vars = which(pvals < .1)
+if (length(good_vars) < 2) {
+    good_vars = 1:ncol(X_train)
+}
+
+set.seed(42)
+fit <- train(X_train[, good_vars],
+                        y_train,
+                        trControl = fitControl,
+                        method = clf_model,
+                        metric='ROC')
+
+preds_class = predict.train(fit, newdata=X_test[, good_vars])
+preds_probs = predict.train(fit, newdata=X_test[, good_vars], type='prob')
+dat = cbind(data.frame(obs = y_test, pred = preds_class), preds_probs)
+mcs = twoClassSummary(dat, lev=colnames(preds_probs))
+test_results = c(mcs['ROC'], mcs['Sens'], mcs['Spec'])
+names(test_results) = c('test_AUC', 'test_Sens', 'test_Spec')
+print(test_results)
+# res = c(phen, clf_model, c1, c2, impute, use_covs,
+#         test_results, table(y_train), table(y_test))
+# line_res = paste(res,collapse=',')
+# write(line_res, file=out_file, append=TRUE)
+
+# # export variable importance
+# a = varImp(fit, useModel=T)
+# b = varImp(fit, useModel=F)
+# split_fname = strsplit(x=out_file, split='/')[[1]]
+# myprefix = gsub(x=split_fname[length(split_fname)], pattern='.csv', replacement='')
+# out_dir = '~/data/baseline_prediction/more_models/'
+# fname = sprintf('%s/varimp_%s_%s_%s_%s_%s_%s_%s.csv',
+#                 out_dir, myprefix, clf_model, phen, c1, c2, impute, use_covs)
+# # careful here because for non-linear models the rows of the importance matrix
+# # are not aligned!!!
+# write.csv(cbind(a$importance, b$importance), file=fname)
+# print(varImp(fit, useModel=F, scale=F))
+
+# # export fit
+# fname = sprintf('%s/fit_%s_%s_%s_%s_%s_%s_%s.RData',
+#                 out_dir, myprefix, clf_model, phen, c1, c2, impute, use_covs)
+# save(fit, file=fname)
+
+# print(line_res)
