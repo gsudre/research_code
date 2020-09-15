5c1737bac8677b7f946c8171389fb592d0af60d5
Gustavo Sudre <gustavo.sudre@nih.gov>
2020-09-15 06:47:19 -0400

    cleaning up Philip's file

--- lab_mgmt/augment_compiled_sx.R
+++ lab_mgmt/augment_compiled_sx.R
@@ -13,7 +13,21 @@ data$DX_dsm5 = NA
 # records in Philip's files and childhood CAADIA records are special
 idx = which(data$source=='PhilipsOldFiles')
 data[idx, 'DX_dsm5'] = data[idx, 'other_dx']
+data[which(data$DX_dsm5 == 'ADD'), 'DX_dsm5'] = 'ADHD'
+data[which(data$DX_dsm5 == 'ADT'), 'DX_dsm5'] = 'ADHD'
+data[which(data$DX_dsm5 == 'ADT(unaffected)'), 'DX_dsm5'] = 'ADHD'
+data[which(data$DX_dsm5 == 'NV(ADDsib)'), 'DX_dsm5'] = 'NV'
+other_dx = c('EMOTIOANL_DYSREG' ,'LD', 'Psychosis_NOS', 'XNV')
+idx = which(data$DX_dsm5 %in% other_dx)
+data[idx, 'DX_dsm5'] = 'other'
 idx = which(data$DOA=='child')
+for (i in idx) {
+    if (data[i, 'SX_inatt'] >= 6 || data[i, 'SX_hi'] >= 6) {
+        data[i, 'DX_dsm5'] = 'ADHD'
+    } else {
+        data[i, 'DX_dsm5'] = 'NV'
+    }
+}
 
 
 today = format(Sys.time(), "%m%d%Y")
